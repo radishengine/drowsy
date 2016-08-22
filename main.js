a58f62d28100da1e49c01d2391694fb2b87a02e4
radishengine <radishengine@gmail.com>
2016-08-22 10:40:05 +0100

    Set up file drag and drop

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
 
-console.log('hello world');
+require([], function() {
+  
+  'use strict';
+  
+  function makeFileDrop(el, callback) {
+    if (typeof el === 'string') {
+      el = document.getElementById(el);
+      if (!el) {
+        console.error('filedrop element not found');
+        return;
+      }
+      el.addEventListener('dragenter', function(e) {
+        el.classList.add('dropping');
+      });
+      el.addEventListener('dragleave', function(e) {
+        el.classList.remove('dropping');
+      });
+      el.addEventListener('dragover', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+      });
+      el.addEventListener('drop', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        el.classList.remove('dropping');
+        if (e.dataTransfer.files[0]) {
+          callback(e.dataTransfer.files[0]);
+        }
+      });
+      el.classList.add('drop-target');
+    }
+  }
+  
+  makeFileDrop('drop-zone', function(droppedFile) {
+    console.log(droppedFile);
+  });
+  
+});
