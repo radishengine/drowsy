a34cbe12fc00ea5e479c036eb2f68cfd35a89717
radishengine <radishengine@gmail.com>
2016-08-25 19:22:13 +0100

    Support 32x32 monochrome icon with mask resources

--- AppleVolume.js
+++ AppleVolume.js
@@ -528,6 +528,28 @@ define(['ByteSource'], function(ByteSource) {
                   var hotspotDV = new DataView(resource.data.buffer, resource.data.byteOffset + 64, 8);
                   resource.hotspot = {y:hotspotDV.getInt16(0), x:hotspotDV.getInt16(2)};
                   break;
+                case 'ICN#':
+                  if (resource.data.length !== 256) {
+                    console.error('ICS# resource expected to be 256 bytes, got ' + resource.data.length);
+                    break;
+                  }
+                  var img = document.createElement('CANVAS');
+                  img.width = 32;
+                  img.height = 32;
+                  var ctx = img.getContext('2d');
+                  var pix = ctx.createImageData(32, 32);
+                  for (var ibyte = 0; ibyte < 128; ibyte++) {
+                    var databyte = resource.data[ibyte], maskbyte = resource.data[128 + ibyte];
+                    for (var ibit = 0; ibit < 8; ibit++) {
+                      var imask = 0x80 >> ibit;
+                      if (maskbyte & imask) {
+                        pix.data.set(databyte & imask ? PIXEL1 : PIXEL0, (ibyte*8 + ibit) * 4);
+                      }
+                    }
+                  }
+                  ctx.putImageData(pix, 0, 0);
+                  resource.image = {url: img.toDataURL(), width:32, height:32};
+                  break;
                 case 'ics#':
                   if (resource.data.length !== 64) {
                     console.error('ics# resource expected to be 64 bytes, got ' + resource.data.length);
