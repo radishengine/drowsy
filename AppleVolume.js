7c6f3fbb4df0f706c5b9e19dab1a6bbdaa31dec2
radishengine <radishengine@gmail.com>
2016-08-26 14:33:29 +0100

    Add skeleton loop for handling PICT v1 resources

--- AppleVolume.js
+++ AppleVolume.js
@@ -797,6 +797,228 @@ define(['ByteSource'], function(ByteSource) {
                   if (resource.data[10] === 0x11 && resource.data[11] === 0x01) {
                     // version 1
                     console.log('PICTv1', left, top, right, bottom);
+                    for (var pictPos = 12; resource.data[pictPos] !== 0xff; ) {
+                      switch(resource.data[pictPos++]) {
+                        case 0x00: break; // no-op
+                        // case 0x01: // clipping region
+                        case 0x02: // background pattern
+                          pictPos += 8;
+                          break;
+                        case 0x03: // font number for text
+                          pictPos += 2;
+                          break;
+                        case 0x04: // text font style
+                          pictPos += 1;
+                          break;
+                        case 0x05: // source mode
+                          pictPos += 2;
+                          break;
+                        case 0x06: // extra space
+                          pictPos += 4;
+                          break;
+                        case 0x07: // pen size
+                          pictPos += 4;
+                          break;
+                        case 0x08: // pen mode
+                          pictPos += 2;
+                          break;
+                        case 0x09: // pen pattern
+                          pictPos += 8;
+                          break;
+                        case 0x0A: // fill pattern
+                          pictPos += 8;
+                          break;
+                        case 0x0B: // oval size
+                          pictPos += 4;
+                          break;
+                        case 0x0C: // origin
+                          pictPos += 4;
+                          break;
+                        case 0x0D: // text size
+                          pictPos += 2;
+                          break;
+                        case 0x0E: // foreground color
+                          pictPos += 4;
+                          break;
+                        case 0x0F: // background color
+                          pictPos += 4;
+                          break;
+                        case 0x10: // txratio
+                          pictPos += 8;
+                          break;
+                        case 0x11: // version
+                          pictPos += 1;
+                          break;
+                        case 0x20: // line
+                          pictPos += 8;
+                          break;
+                        case 0x21: // line from
+                          pictPos += 4;
+                          break;
+                        case 0x22: // short line
+                          pictPos += 6;
+                          break;
+                        case 0x23: // short line from
+                          pictPos += 2;
+                          break;
+                        // case 0x28: // long text
+                        // case 0x29: // DHtext
+                        // case 0x2A: // DVtext
+                        // case 0x2B: // DHDVtext
+                        case 0x30: // frame rect
+                          pictPos += 8;
+                          break;
+                        case 0x31: // paint rect
+                          pictPos += 8;
+                          break;
+                        case 0x32: // erase rect
+                          pictPos += 8;
+                          break;
+                        case 0x33: // invert rect
+                          pictPos += 8;
+                          break;
+                        case 0x34: // fill rect
+                          pictPos += 8;
+                          break;
+                        case 0x38: // frame same rect
+                          break;
+                        case 0x39: // paint same rect
+                          break;
+                        case 0x3A: // erase same rect
+                          break;
+                        case 0x3B: // invert same rect
+                          break;
+                        case 0x3C: // fill same rect
+                          break;
+                          
+                        case 0x40: // frame rrect
+                          pictPos += 8;
+                          break;
+                        case 0x41: // paint rrect
+                          pictPos += 8;
+                          break;
+                        case 0x42: // erase rrect
+                          pictPos += 8;
+                          break;
+                        case 0x43: // invert rrect
+                          pictPos += 8;
+                          break;
+                        case 0x44: // fill rrect
+                          pictPos += 8;
+                          break;
+                        case 0x48: // frame same rrect
+                          break;
+                        case 0x49: // paint same rrect
+                          break;
+                        case 0x4A: // erase same rrect
+                          break;
+                        case 0x4B: // invert same rrect
+                          break;
+                        case 0x4C: // fill same rrect
+                          break;
+                          
+                        case 0x50: // frame oval
+                          pictPos += 8;
+                          break;
+                        case 0x51: // paint oval
+                          pictPos += 8;
+                          break;
+                        case 0x52: // erase oval
+                          pictPos += 8;
+                          break;
+                        case 0x53: // invert oval
+                          pictPos += 8;
+                          break;
+                        case 0x54: // fill oval
+                          pictPos += 8;
+                          break;
+                        case 0x58: // frame same oval
+                          break;
+                        case 0x59: // paint same oval
+                          break;
+                        case 0x5A: // erase same oval
+                          break;
+                        case 0x5B: // invert same oval
+                          break;
+                        case 0x5C: // fill same oval
+                          break;
+                          
+                        case 0x60: // frame arc
+                          pictPos += 12;
+                          break;
+                        case 0x61: // paint arc
+                          pictPos += 12;
+                          break;
+                        case 0x62: // erase arc
+                          pictPos += 12;
+                          break;
+                        case 0x63: // invert arc
+                          pictPos += 12;
+                          break;
+                        case 0x64: // fill arc
+                          pictPos += 12;
+                          break;
+                        case 0x68: // frame same arc
+                          pictPos += 4;
+                          break;
+                        case 0x69: // paint same arc
+                          pictPos += 4;
+                          break;
+                        case 0x6A: // erase same arc
+                          pictPos += 4;
+                          break;
+                        case 0x6B: // invert same arc
+                          pictPos += 4;
+                          break;
+                        case 0x6C: // fill same arc
+                          pictPos += 4;
+                          break;
+                          
+                        // case 0x70: // frame poly
+                        // case 0x71: // paint poly
+                        // case 0x72: // erase poly
+                        // case 0x73: // invert poly
+                        // case 0x74: // fill poly
+                        case 0x78: // frame same poly
+                          break;
+                        case 0x79: // paint same poly
+                          break;
+                        case 0x7A: // erase same poly
+                          break;
+                        case 0x7B: // invert same poly
+                          break;
+                        case 0x7C: // fill same poly
+                          break;
+                        
+                        // case 0x80: // frame region
+                        // case 0x81: // paint region
+                        // case 0x82: // erase region
+                        // case 0x83: // invert region
+                        // case 0x84: // fill region
+                        case 0x88: // frame same region
+                          break;
+                        case 0x89: // paint same region
+                          break;
+                        case 0x8A: // erase same region
+                          break;
+                        case 0x8B: // invert same region
+                          break;
+                        case 0x8C: // fill same region
+                          break;
+                        
+                        //case 0x90: // copy bits to clipped rect
+                        //case 0x91: // copy bits to clipped region
+                        //case 0x98: // copy packed bits to clipped rect
+                        //case 0x99: // copy packed bits to clipped region
+                        
+                        case 0xA0: // short comment
+                          pictPos += 2;
+                          break;
+                        //case 0xA1: // long comment
+                        
+                        //case 0xFF: // end of picture (checked by outer loop)
+                      }
+                    }
                   }
                   else if (resource.data[10] === 0x00 && resource.data[11] === 0x11
                       && resource.data[12] === 0x02 && resource.data[13] === 0xff) {
