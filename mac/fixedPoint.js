0e8b715960f1a20f2fe5b4c97900e4b3059ac766
radishengine <radishengine@gmail.com>
2016-08-27 17:15:50 +0100

    Fix mistake

--- mac/fixedPoint.js
+++ mac/fixedPoint.js
@@ -3,7 +3,7 @@ define(function() {
   'use strict';
   
   return {
-    fromInt32: fixed(i32) {
+    fromInt32: function(i32) {
       var frac = 0;
       for (var i = 0; i < 16; i++) {
         if (i32 & (0x8000 >> i)) {
