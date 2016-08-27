dd598cfb8283e5fd59dc7b2b0e43138a0655c107
radishengine <radishengine@gmail.com>
2016-08-27 19:19:38 +0100

    Check that a script failure was a nodefine

--- AppleVolume.js
+++ AppleVolume.js
@@ -510,13 +510,15 @@ define(['ByteSource', 'mac/roman'], function(ByteSource, macintoshRoman) {
                         }
                       },
                       function(err) {
-                        requirejs.undef(importString);
-                        define(importString,
-                          // do-nothing handler
-                          function() {
-                            return function(resource) {
-                            };
-                          });
+                        if (err.requireType === 'nodefine') {
+                          requirejs.undef(importString);
+                          define(importString,
+                            // do-nothing handler
+                            function() {
+                              return function(resource) {
+                              };
+                            });
+                        }
                         if (typeof reader.onresource === 'function') {
                           reader.onresource(resource);
                         }
