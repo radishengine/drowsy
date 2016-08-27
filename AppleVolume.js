f3136d5c51398cfdb6c6157b88788a9b50a3b7c2
radishengine <radishengine@gmail.com>
2016-08-27 11:45:05 +0100

    Use an object instead of an array for VWLB data

--- AppleVolume.js
+++ AppleVolume.js
@@ -734,21 +734,19 @@ define(['ByteSource'], function(ByteSource) {
                   break;
                 case 'VWLB':
                   var VWLB = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-                  resource.dataObject = new Array(VWLB.getUint16(0, false));
-                  var textBase = 2 + 4 * (resource.dataObject.length + 1);
-                  var totalTextLen = VWLB.getUint16(2 + (4 * resource.dataObject.length) + 2, false);
-                  resource.dataObject.text = macintoshRoman(resource.data, textBase, totalTextLen);
-                  var names = resource.dataObject.text.match(/^[^\r\n]*/)[0];
-                  for (var imarker = 0; imarker < resource.dataObject.length; imarker++) {
+                  var entries = new Array(VWLB.getUint16(0, false));
+                  var textBase = 2 + 4 * (entries.length + 1);
+                  var totalTextLen = VWLB.getUint16(2 + (4 * entries.length) + 2, false);
+                  entries.text = macintoshRoman(resource.data, textBase, totalTextLen);
+                  var names = entries.text.match(/^[^\r\n]*/)[0];
+                  resource.dataObject = {};
+                  for (var imarker = 0; imarker < entries.length; imarker++) {
                     var markerBase = 2 + (4 * imarker);
-                    resource.dataObject[imarker] = {
-                      frame: VWLB.getUint16(markerBase, false),
-                      name: names.substring(
+                    resource.dataObject[names.substring(
                         VWLB.getUint16(markerBase + 2, false),
                         VWLB.getUint16(markerBase + 4 + 2, false))
-                    };
+                    }] = VWLB.getUint16(markerBase, false);
                   }
-                  delete resource.dataObject.text;
                   break;
                 case 'BITD':
                   resource.getUnpackedData = function() {
