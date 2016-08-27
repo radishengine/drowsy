c8de84b1f4b56666f0ec9ef286d6e1767d2b2623
radishengine <radishengine@gmail.com>
2016-08-27 16:20:31 +0100

    Create resource handler script for icl8

---
+++ mac/resources/open_icl8.js
@@ -0,0 +1,22 @@
+define(function() {
+
+  'use strict';
+  
+  return function(resource) {
+    if (resource.data.length !== 1024) {
+      console.error('icl8 resource expected to be 1024 bytes, got ' + resource.data.length);
+      return;
+    }
+    var img = document.createElement('CANVAS');
+    img.width = 32;
+    img.height = 32;
+    var ctx = img.getContext('2d');
+    var pix = ctx.createImageData(32, 32);
+    for (var ibyte = 0; ibyte < 1024; ibyte++) {
+      pix.data.set(mac8BitSystemPalette[resource.data[ibyte]], ibyte*4);
+    }
+    ctx.putImageData(pix, 0, 0);
+    resource.image = {url: img.toDataURL(), width:32, height:32};
+  };
+
+});
