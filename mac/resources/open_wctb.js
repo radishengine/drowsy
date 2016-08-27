c2b6d8fa6eb5fed8a40fbd64399b92acc4f33332
radishengine <radishengine@gmail.com>
2016-08-28 00:12:49 +0100

    Use null dataObject if color table is empty

--- mac/resources/open_wctb.js
+++ mac/resources/open_wctb.js
@@ -6,29 +6,28 @@ define(function() {
     if (entryCount < 0) {
       console.error('color table resource: invalid number of entries');
     }
+    if (entryCount === 0) {
+      resource.dataObject = null;
+      return;
+    }
     var palCanvas = document.createElement('CANVAS');
     palCanvas.width = entryCount;
-    if (entryCount === 0) {
-      palCanvas.height = 0;
-    }
-    else {
-      palCanvas.height = 1;
-      var palCtx = palCanvas.getContext('2d');
-      var palData = palCtx.createImageData(entryCount, 1);
-      for (var icolor = 0; icolor < entryCount; icolor++) {
-        var offset = dv.getInt16(8 + icolor*8, false) * 4;
-        if (offset >= 0) {
-          palData.data[offset] = resource.data[8 + icolor*8 + 2];
-          palData.data[offset + 1] = resource.data[8 + icolor*8 + 4];
-          palData.data[offset + 2] = resource.data[8 + icolor*8 + 6];
-          palData.data[offset + 3] = 255;
-        }
+    palCanvas.height = 1;
+    var palCtx = palCanvas.getContext('2d');
+    var palData = palCtx.createImageData(entryCount, 1);
+    for (var icolor = 0; icolor < entryCount; icolor++) {
+      var offset = dv.getInt16(8 + icolor*8, false) * 4;
+      if (offset >= 0) {
+        palData.data[offset] = resource.data[8 + icolor*8 + 2];
+        palData.data[offset + 1] = resource.data[8 + icolor*8 + 4];
+        palData.data[offset + 2] = resource.data[8 + icolor*8 + 6];
+        palData.data[offset + 3] = 255;
       }
-      palCtx.putImageData(palData, 0, 0);
     }
+    palCtx.putImageData(palData, 0, 0);
     resource.image = {
-      width: palCanvas.width,
-      height: palCanvas.height,
+      width: entryCount,
+      height: 1,
       url: palCanvas.toDataURL(),
     };
 
