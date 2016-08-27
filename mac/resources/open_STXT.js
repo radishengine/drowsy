282aeae9c93c9a170817e2c042c82d956dc1b9a6
radishengine <radishengine@gmail.com>
2016-08-27 15:58:10 +0100

    Create resource handler script for STXT

---
+++ mac/resources/open_STXT.js
@@ -0,0 +1,13 @@
+define(function() {
+
+  'use strict';
+  
+  return function(resource) {
+    var dataDV = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
+		var textPos = dataDV.getUint32(0, false);
+		var textLen = dataDV.getUint32(4, false);
+		var num3 = dataDV.getUint32(8, false); // TODO: what is this
+		resource.text = macintoshRoman(resource.data, textPos, textLen);
+  };
+
+});
