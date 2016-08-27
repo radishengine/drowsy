c01a966b5a9019e1b1d47915d94584f1f8b2ccb6
radishengine <radishengine@gmail.com>
2016-08-27 15:59:23 +0100

    Create resource handler script for vers

---
+++ mac/resources/open_vers.js
@@ -0,0 +1,26 @@
+define(function() {
+
+  'use strict';
+  
+  return function() {
+    resource.dataObject = {
+      major: resource.data[0],
+      minor: resource.data[1],
+      developmentStage: (function(v) {
+        switch(v) {
+          case 0x20: return 'development';
+          case 0x40: return 'alpha';
+          case 0x60: return 'beta';
+          case 0x80: return 'release';
+          default: return v;
+        }
+      })(resource.data[2]),
+      prereleaseRevisionLevel: resource.data[3],
+      regionCode: (resource.data[4] << 8) | resource.data[5],
+    };
+    resource.dataObject.versionNumber = macintoshRoman(resource.data, 7, resource.data[6]);
+    var pos = 7 + resource.data[6];
+    resource.dataObject.versionMessage = macintoshRoman(resource.data, pos + 1, resource.data[pos]);
+  };
+
+});
