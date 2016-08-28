47239e8a1450f49dfe8d3974707d37b3f488bd1e
radishengine <radishengine@gmail.com>
2016-08-28 15:11:04 +0100

    Create resource handler script for DLOG

---
+++ mac/resources/open_DLOG.js
@@ -0,0 +1,30 @@
+define(['mac/roman'], function(macintoshRoman) {
+
+  'use strict';
+  
+  return function(resource) {
+    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
+    resource.dataObject = {
+      rectangle: {
+        top: dv.getInt16(0, false),
+        left: dv.getInt16(2, false),
+        bottom: dv.getInt16(4, false),
+        right: dv.getInt16(6, false),
+      },
+      type: dv.getUint16(8, false),
+      visible: !!resource.data[10],
+      closeBox: !!resource.data[12],
+      referenceConstant: dv.getInt32(14, false),
+      itemListResourceID: dv.getInt16(18, false),
+    };
+    switch(resource.dataObject.type) {
+      case 0: resource.dataObject.type = 'modal'; break;
+      case 4: resource.dataObject.type = 'modeless'; break;
+      case 5: resource.dataObject.type = 'movableModal'; break;
+    }
+    resource.dataObject.text = macintoshRoman(resource.data, 21, resource.data[20]);
+    var pos = 21 + resource.dataObject.text.length + resource.dataObject.text.length % 2;
+    resource.dataObject.positionCode = dv.getUint16(pos, false);
+  };
+
+});
