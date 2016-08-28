3080c460c5126293d3902fc9f6f208fabc691fb0
radishengine <radishengine@gmail.com>
2016-08-28 19:29:31 +0100

    Create resource handler script for 8BIM

---
+++ mac/resources/open_8BIM.js
@@ -0,0 +1,218 @@
+define(['mac/roman'], function(macintoshRoman) {
+
+  'use strict';
+  
+  return function(resource) {
+    var name = macintoshRoman(resource.data, 1, resource.data[0]);
+    var pos = 1 + name.length + (1 + name.length) % 2;
+    var dataLen = new DataView(resource.data.buffer, resource.data.byteOffset + pos, 4).getUint32(0, false);
+    var data = resource.data.subarray(pos + 4, pos + 4 + dataLen);
+    resource.dataObject = {
+      name: name,
+      dataLen: dataLen,
+      data: [].slice.apply(data),
+    };
+    switch(resource.id) {
+      case 1000: // 2-byte values: number of channels, rows, columns, depth, mode
+        break;
+      case 1001: // Macintosh print manager print info record
+        break;
+      case 1002: // Macintosh page format info
+        break;
+      case 1003: // indexed color table
+        break;
+      case 1005: // ResolutionInfo structure (Appendix A in Photoshop API Guide.pdf)
+        break;
+      case 1006: // Names of the alpha channels as a series of Pascal strings.
+        break;
+      case 1007: // ID 1077 DisplayInfo structure. See Appendix A in Photoshop API Guide.pdf.
+        break;
+      case 1008: // The caption as a Pascal string.
+        break;
+      case 1009:
+        // Border info
+        // fixed number (2 bytes real, 2 bytes fraction) for the border width
+        // 2 bytes for border units (1 = inches, 2 = cm, 3 = points, 4 = picas, 5 = columns)
+        break;
+      case 1010: // Background color. See See Color structure.
+        break;
+      case 1011:
+        // Print flags
+        // one-byte booleans (see Page Setup dialog):
+        // labels, crop marks, color bars, registration marks, negative, flip, interpolate, caption, print flags
+      case 1012: // Grayscale and multichannel halftoning information
+        break;
+      case 1013: // Color halftoning information
+        break;
+      case 1014: // Duotone halftoning information
+        break;
+      case 1015: // Grayscale and multichannel transfer function
+        break;
+      case 1016: // Color transfer functions
+        break;
+      case 1017: // Duotone transfer functions
+        break;
+      case 1018: // Duotone image information
+        break;
+      case 1019: // Two bytes for the effective black and white values for the dot range
+        break;
+      case 1020: // (Obsolete)
+        break;
+      case 1021: // EPS options
+        break;
+      case 1022: // Quick Mask information. 2 bytes containing Quick Mask channel ID; 1- byte boolean indicating whether the mask was initially empty.
+        break;
+      case 1023: // (Obsolete)
+        break;
+      case 1024: // Layer state information. 2 bytes containing the index of target layer (0 = bottom layer).
+        break;
+      case 1025: // Working path (not saved). See See Path resource format.
+        break;
+      case 1026: // Layers group information. 2 bytes per layer containing a group ID for the dragging groups. Layers in a group have the same group ID.
+        break;
+      case 1027: // (Obsolete)
+        break;
+      case 1028: // IPTC-NAA record. Contains the File Info... information. See the documentation in the IPTC folder of the Documentation folder.
+        break;
+      case 1029: // Image mode for raw format files
+        break;
+      case 1030: // JPEG quality. Private.
+        break;
+      case 1032: // (Photoshop 4.0) Grid and guides information. See See Grid and guides resource format.
+        break;
+      case 1033: // (Photoshop 4.0) Thumbnail resource for Photoshop 4.0 only. See See Thumbnail resource format.
+        break;
+      case 1034: // (Photoshop 4.0) Copyright flag. Boolean indicating whether image is copyrighted. Can be set via Property suite or by user in File Info...
+        break;
+      case 1035: // (Photoshop 4.0) URL. Handle of a text string with uniform resource locator. Can be set via Property suite or by user in File Info...
+        break;
+      case 1036: // (Photoshop 5.0) Thumbnail resource (supersedes resource 1033). See See Thumbnail resource format.
+        break;
+      case 1037: // (Photoshop 5.0) Global Angle. 4 bytes that contain an integer between 0 and 359, which is the global lighting angle for effects layer. If not present, assumed to be 30.
+        break;
+      case 1038: // (Obsolete) See ID 1073 below. (Photoshop 5.0) Color samplers resource. See See Color samplers resource format.
+        break;
+      case 1039: // (Photoshop 5.0) ICC Profile. The raw bytes of an ICC (International Color Consortium) format profile. See ICC1v42_2006-05.pdf in the Documentation folder and icProfileHeader.h in Sample Code\Common\Includes .
+        break;
+      case 1040: // (Photoshop 5.0) Watermark. One byte.
+        break;
+      case 1041: // (Photoshop 5.0) ICC Untagged Profile. 1 byte that disables any assumed profile handling when opening the file. 1 = intentionally untagged.
+        break;
+      case 1042: // (Photoshop 5.0) Effects visible. 1-byte global flag to show/hide all the effects layer. Only present when they are hidden.
+        break;
+      case 1043: // (Photoshop 5.0) Spot Halftone. 4 bytes for version, 4 bytes for length, and the variable length data.
+        break;
+      case 1044: // (Photoshop 5.0) Document-specific IDs seed number. 4 bytes: Base value, starting at which layer IDs will be generated (or a greater value if existing IDs already exceed it). Its purpose is to avoid the case where we add layers, flatten, save, open, and then add more layers that end up with the same IDs as the first set.
+        break;
+      case 1045: // (Photoshop 5.0) Unicode Alpha Names. Unicode string
+        break;
+      case 1046: // (Photoshop 6.0) Indexed Color Table Count. 2 bytes for the number of colors in table that are actually defined
+        break;
+      case 1047: // (Photoshop 6.0) Transparency Index. 2 bytes for the index of transparent color, if any.
+        break;
+      case 1049: // (Photoshop 6.0) Global Altitude. 4 byte entry for altitude
+        break;
+      case 1050: // (Photoshop 6.0) Slices. See See Slices resource format.
+        break;
+      case 1051: // (Photoshop 6.0) Workflow URL. Unicode string
+        break;
+      case 1052: // (Photoshop 6.0) Jump To XPEP. 2 bytes major version, 2 bytes minor version, 4 bytes count. Following is repeated for count: 4 bytes block size, 4 bytes key, if key = 'jtDd' , then next is a Boolean for the dirty flag; otherwise it's a 4 byte entry for the mod date.
+        break;
+      case 1053: // (Photoshop 6.0) Alpha Identifiers. 4 bytes of length, followed by 4 bytes each for every alpha identifier.
+        break;
+      case 1054: // (Photoshop 6.0) URL List. 4 byte count of URLs, followed by 4 byte long, 4 byte ID, and Unicode string for each count.
+        break;
+      case 1057: // (Photoshop 6.0) Version Info. 4 bytes version, 1 byte hasRealMergedData , Unicode string: writer name, Unicode string: reader name, 4 bytes file version.
+        break;
+      case 1058: // (Photoshop 7.0) EXIF data 1. See http://www.kodak.com/global/plugins/acrobat/en/service/digCam/exifStandard2.pdf
+        break;
+      case 1059: // (Photoshop 7.0) EXIF data 3. See http://www.kodak.com/global/plugins/acrobat/en/service/digCam/exifStandard2.pdf
+        break;
+      case 1060: // (Photoshop 7.0) XMP metadata. File info as XML description. See http://www.adobe.com/devnet/xmp/
+        break;
+      case 1061: // (Photoshop 7.0) Caption digest. 16 bytes: RSA Data Security, MD5 message-digest algorithm
+        break;
+      case 1062: // (Photoshop 7.0) Print scale. 2 bytes style (0 = centered, 1 = size to fit, 2 = user defined). 4 bytes x location (floating point). 4 bytes y location (floating point). 4 bytes scale (floating point)
+        break;
+      case 1064: // (Photoshop CS) Pixel Aspect Ratio. 4 bytes (version = 1 or 2), 8 bytes double, x / y of a pixel. Version 2, attempting to correct values for NTSC and PAL, previously off by a factor of approx. 5%.
+        break;
+      case 1065: // (Photoshop CS) Layer Comps. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure)
+        break;
+      case 1066: // (Photoshop CS) Alternate Duotone Colors. 2 bytes (version = 1), 2 bytes count, following is repeated for each count: [ Color: 2 bytes for space followed by 4 * 2 byte color component ], following this is another 2 byte count, usually 256, followed by Lab colors one byte each for L, a, b. This resource is not read or used by Photoshop.
+        break;
+      case 1067: // (Photoshop CS)Alternate Spot Colors. 2 bytes (version = 1), 2 bytes channel count, following is repeated for each count: 4 bytes channel ID, Color: 2 bytes for space followed by 4 * 2 byte color component. This resource is not read or used by Photoshop.
+        break;
+      case 1069: // (Photoshop CS2) Layer Selection ID(s). 2 bytes count, following is repeated for each count: 4 bytes layer ID
+        break;
+      case 1070: // (Photoshop CS2) HDR Toning information
+        break;
+      case 1071: // (Photoshop CS2) Print info
+        break;
+      case 1072: // (Photoshop CS2) Layer Group(s) Enabled ID. 1 byte for each layer in the document, repeated by length of the resource. NOTE: Layer groups have start and end markers
+        break;
+      case 1073: // (Photoshop CS3) Color samplers resource. Also see ID 1038 for old format. See See Color samplers resource format.
+        break;
+      case 1074: // (Photoshop CS3) Measurement Scale. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure)
+        break;
+      case 1075: // (Photoshop CS3) Timeline Information. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure)
+        break;
+      case 1076: // (Photoshop CS3) Sheet Disclosure. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure)
+        break;
+      case 1077: // (Photoshop CS3) DisplayInfo structure to support floating point clors. Also see ID 1007. See Appendix A in Photoshop API Guide.pdf .
+        break;
+      case 1078: // (Photoshop CS3) Onion Skins. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure)
+        break;
+      case 1080: // (Photoshop CS4) Count Information. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure) Information about the count in the document. See the Count Tool.
+        break;
+      case 1082: // (Photoshop CS5) Print Information. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure) Information about the current print settings in the document. The color management options.
+        break;
+      case 1083: // (Photoshop CS5) Print Style. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure) Information about the current print style in the document. The printing marks, labels, ornaments, etc.
+        break;
+      case 1084: // (Photoshop CS5) Macintosh NSPrintInfo. Variable OS specific info for Macintosh. NSPrintInfo. It is recommened that you do not interpret or use this data.
+        break;
+      case 1085: // (Photoshop CS5) Windows DEVMODE. Variable OS specific info for Windows. DEVMODE. It is recommened that you do not interpret or use this data.
+        break;
+      case 1086: // (Photoshop CS6) Auto Save File Path. Unicode string. It is recommened that you do not interpret or use this data.
+        break;
+      case 1087: // (Photoshop CS6) Auto Save Format. Unicode string. It is recommened that you do not interpret or use this data.
+        break;
+      case 1088: // (Photoshop CC) Path Selection State. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure) Information about the current path selection state.
+        break;
+/*
+      case 2000-2997: // Path Information (saved paths). See See Path resource format.
+*/
+      case 2999: // Name of clipping path. See See Path resource format.
+        break;
+      case 3000: // (Photoshop CC) Origin Path Info. 4 bytes (descriptor version = 16), Descriptor (see See Descriptor structure) Information about the origin path data.
+        break;
+/*
+      case 4000-4999: // Plug-In resource(s). Resources added by a plug-in. See the plug-in API found in the SDK documentation
+*/
+      case 7000: // Image Ready variables. XML representation of variables definition
+        break;
+      case 7001: // Image Ready data sets
+        break;
+      case 7002: // Image Ready default selected state
+        break;
+      case 7003: // Image Ready 7 rollover expanded state
+        break;
+      case 7004: // Image Ready rollover expanded state
+        break;
+      case 7005: // Image Ready save layer settings
+        break;
+      case 7006: // Image Ready version
+        break;
+      case 8000: // (Photoshop CS3) Lightroom workflow, if present the document is in the middle of a Lightroom workflow.
+        break;
+      case 10000:
+        // Print flags:
+        // 2 bytes version ( = 1)
+        // 1 byte center crop marks
+        // 1 byte ( = 0)
+        // 4 bytes bleed width value
+        // 2 bytes bleed width scale
+        break;
+    }
+  };
+
+});
