d8c7d92bdc8b559fbfb1a33c66ce5853a4c6e979
radishengine <radishengine@gmail.com>
2016-08-28 18:32:28 +0100

    With no complex shape, use a dataObject instead

--- mac/resources/open_RGN .js
+++ mac/resources/open_RGN .js
@@ -14,44 +14,42 @@ define(function() {
       bottom: dv.getInt16(6, false),
       right: dv.getInt16(8, false),
     };
+    if (resource.data.length === 10) {
+      resource.dataObject = bounds;
+      return;
+    }
     var canvas = document.createElement('CANVAS');
     canvas.width = bounds.right - bounds.left;
     canvas.height = bounds.bottom - bounds.top;
     var ctx = canvas.getContext('2d');
-    if (resource.data.length === 10) {
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-    else {
-      ctx.fillStyle = 'white';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = 'black';
-      var on = false;
-      var y = -1;
-      for (var pos = 10; pos < resource.data.length; ) {
-        var newY = dv.getUint16(pos, false);
-        if (newY === 0x7fff) {
-          break;
-        }
-        if (on && newY > ++y) {
-          ctx.drawRect(0, y, canvas.width, newY - y);
-        }
-        y = newY;
-        pos += 2;
-        var x = 0;
-        var runLength = dv.getUint16(pos, false);
-        while (runLength !== 0x7fff) {
-          if (on) ctx.fillRect(x, y, runLength, 1);
-          on = !on;
-          x += runLength;
-          pos += 2;
-          runLength = dv.getUint16(pos, false);
-        }
-        if (on) {
-          ctx.fillRect(x, y, canvas.width - x, 1);
-        }
-        pos += 2;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    var on = false;
+    var y = -1;
+    for (var pos = 10; pos < resource.data.length; ) {
+      var newY = dv.getUint16(pos, false);
+      if (newY === 0x7fff) {
+        break;
       }
+      if (on && newY > ++y) {
+        ctx.drawRect(0, y, canvas.width, newY - y);
+      }
+      y = newY;
+      pos += 2;
+      var x = 0;
+      var runLength = dv.getUint16(pos, false);
+      while (runLength !== 0x7fff) {
+        if (on) ctx.fillRect(x, y, runLength, 1);
+        on = !on;
+        x += runLength;
+        pos += 2;
+        runLength = dv.getUint16(pos, false);
+      }
+      if (on) {
+        ctx.fillRect(x, y, canvas.width - x, 1);
+      }
+      pos += 2;
     }
     resource.image = {
       offsetX: bounds.left,
