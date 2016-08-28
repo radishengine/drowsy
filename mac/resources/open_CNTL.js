aef0e4d16ec31cdfa503d2d4e5e5637e6fd709e0
radishengine <radishengine@gmail.com>
2016-08-28 22:07:46 +0100

    Create resource handler script for CNTL

---
+++ mac/resources/open_CNTL.js
@@ -0,0 +1,25 @@
+define(['mac/roman'], function(macintoshRoman) {
+
+  'use strict';
+  
+  return function(resource) {
+    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
+    resource.dataObject = {
+      rectangle: {
+        top: dv.getInt16(0, false),
+        left: dv.getInt16(2, false),
+        bottom: dv.getInt16(4, false),
+        right: dv.getInt16(6, false),
+      },
+      initialSetting: dv.getUint16(8, false),
+      visible: !!resource.data[10],
+      closeBox: !!resource.data[11],
+      maximumSetting: dv.getInt16(12, false),
+      minimumSetting: dv.getInt16(14, false),
+      cdefID: dv.getInt16(16, false),
+      referenceConstant: dv.getInt32(18, false),
+      text: macintoshRoman(resource.data, 23, resource.data[22]);
+    };
+  };
+
+});
