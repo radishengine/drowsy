fadbb775dd5f03fae47927ee4b918b891f86f40a
radishengine <radishengine@gmail.com>
2016-09-03 15:40:23 +0100

    Flesh out methods

--- mac/hfs/BTreeByteSink.js
+++ mac/hfs/BTreeByteSink.js
@@ -9,15 +9,56 @@ define(['mac/hfs/BTreeNodeView'], function(BTreeNodeView) {
   }
   BTreeByteSink.prototype = {
     getRawNode: function(number) {
-      var offset = number * BLOCK_BYTES;
-      var length = BLOCK_BYTES;
-      return this.byteSource.slice(offset, offset+length).getBytes();
+      return this.byteSource.slice(
+        number * BLOCK_BYTES,
+        (number + 1) * BLOCK_BYTES).getBytes();
+    },
+    getNode: function(number) {
+      return this.getRawNode()
+      .then(function(rawNode) {
+        return new BTreeNodeView(rawNode.buffer, rawNode.byteOffset, rawNode.byteLength);
+      });
+    },
+    getHeaderNode: function() {
+      var promise = this.getNode(0)
+      .then(function(node) {
+        if (node.nodeType !== 'header') return Promise.reject('node zero is not a header node');
+        return node;
+      });
+      this.getHeaderNode = function(){ return promise; };
+      return promise;
+    },
+    getFirstLeafNode: function() {
+      var self = this;
+      var promise = this.getHeaderNode()
+      .then(function(node) {
+        return self.getNode(node.records[0].firstLeaf);
+      });
+      this.getFirstLeafNode = function(){ return promise; };
+      return promise;
     },
     getRootNode: function() {
-      return this.getRawNode()
-        .then(function(rootBytes) {
-          return new BTreeNodeView(rootBytes.buffer, rootBytes.byteOffset);
-        });
+      var self = this;
+      var promise = this.getHeaderNode()
+      .then(function(node) {
+        return self.getNode(node.records[0].rootNodeNumber);
+      });
+      this.getRootNode = function(){ return promise; };
+      return promise;
+    },
+    findLeafForParentDirectoryID: function(parentDirectoryID) {
+      var self = this;
+      function onNode(node) {
+        if (node.nodeType === 'leaf') return node;
+        if (node.nodeType !== 'index') return Promise.reject('node is not a leaf or index');
+        for (var i = node.records.length - 1; i >= 0; i--) {
+          if (parentDirectoryID >= node.records[i].parentDirectoryID) {
+            return self.getNode(records[i].nodeNumber).then(onNode);
+          }
+        }
+        return Promise.reject('parent directory ID not found: ' + parentDirectoryID);
+      }
+      return this.getRootNode().then(onNode);
     },
   };
   
