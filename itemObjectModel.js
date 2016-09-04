b0caede56f53a353f306eaf419f587537dc5a2c6
radishengine <radishengine@gmail.com>
2016-09-04 17:58:05 +0100

    Setting item byteSource creates download link

--- itemObjectModel.js
+++ itemObjectModel.js
@@ -2,6 +2,8 @@ define(function() {
 
   'use strict';
   
+  var _BYTESOURCE = new Symbol('byteSource');
+  
   var itemObjectModel = {
     createItem: function(title) {
       var itemElement = document.createElement('SECTION');
@@ -13,6 +15,8 @@ define(function() {
       Object.defineProperties(itemElement, this.itemProperties);
       
       if (title) itemElement.itemTitle = title;
+      
+      itemElement.addEventListener('click', clickItem);
 
       return itemElement;
     },
@@ -26,8 +30,43 @@ define(function() {
         },
         enumerable: true,
       },
+      byteSource: {
+        get: function() {
+          return this[_BYTESOURCE];
+        },
+        set: function(byteSource) {
+          this[_BYTESOURCE] = byteSource;
+          if (!byteSource) {
+            this.titleElement.innerText = this.titleTextElement.innerText;
+            this.titleTextElement = this.titleElement;
+          }
+          else {
+            var link = document.createElement('A');
+            link.href = '#';
+            link.classList.add('item-download');
+            link.download = this.itemTitle.replace(/[\\\/:"<>\*\?\|]/g, '_');
+            link.innerText = this.titleTextElement.innerText;
+            this.titleElement.innerHTML = '';
+            this.titleElement.appendChild(this.titleTextElement = link);
+          }
+        },
+      },
     },
   };
+  
+  function clickItem(e) {
+    e.stopPropagation();
+    if (e.target.classList.contains('item-download')) {
+      if (e.target.href === '#') {
+        e.preventDefault();
+        this.byteSource.getURL().then(function(url) {
+          e.target.href = url;
+          e.target.click();
+        });
+      }
+      return;
+    }
+  }
   
   return itemObjectModel;
 
