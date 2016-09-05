2c45ebd8a7c8229606568cda8c046ee54660e119
radishengine <radishengine@gmail.com>
2016-09-05 15:37:21 +0100

    Update icl4 to use new loader format

--- mac/resources/open_icl4.js
+++ mac/resources/open_icl4.js
@@ -2,22 +2,18 @@ define(['mac/palette16'], function(palette) {
 
   'use strict';
 
-  return function(resource) {
-    if (resource.data.length !== 512) {
-      console.error('icl4 resource expected to be 512 bytes, got ' + resource.data.length);
-      return;
-    }
-    var img = document.createElement('CANVAS');
-    img.width = 32;
-    img.height = 32;
-    var ctx = img.getContext('2d');
-    var pix = ctx.createImageData(32, 32);
-    for (var ibyte = 0; ibyte < 512; ibyte++) {
-      pix.data.set(palette[resource.data[ibyte] >> 4], ibyte*8);
-      pix.data.set(palette[resource.data[ibyte] & 15], ibyte*8 + 4);
-    }
-    ctx.putImageData(pix, 0, 0);
-    resource.image = {url: img.toDataURL(), width:32, height:32};
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      if (bytes.length !== 512) {
+        return Promise.reject('icl4 resource expected to be 512 bytes, got ' + bytes.length);
+      }
+      item.withPixels(32, 32, function(pixelData) {
+        for (var ibyte = 0; ibyte < 512; ibyte++) {
+          pixelData.set(palette[bytes[ibyte] >> 4], ibyte*8);
+          pixelData.set(palette[bytes[ibyte] & 15], ibyte*8 + 4);
+        }
+      });
+    });
   };
 
 });
