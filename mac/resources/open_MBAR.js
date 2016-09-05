610943ed968a38093f3272e42c944e0c59770d9f
radishengine <radishengine@gmail.com>
2016-09-05 15:11:09 +0100

    Change MBAR to new loader format

--- mac/resources/open_MBAR.js
+++ mac/resources/open_MBAR.js
@@ -2,17 +2,19 @@ define(function() {
 
   'use strict';
   
-  return function(resource) {
-    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    var count = dv.getInt16(0, false);
-    if (count < 0) {
-      console.error('MBAR: invalid number of menus');
-      return;
-    }
-    resource.dataObject = new Array(count);
-    for (var i = 0; i < count; i++) {
-      resource.dataObject[i] = dv.getInt16(2 + 2*i);
-    }
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var count = dv.getInt16(0, false);
+      if (count < 0) {
+        return Promise.reject('MBAR: invalid number of menus');
+      }
+      var dataObject = new Array(count);
+      for (var i = 0; i < count; i++) {
+        dataObject[i] = dv.getInt16(2 + 2*i);
+      }
+      resource.setDataObject(dataObject);
+    });
   };
 
 });
