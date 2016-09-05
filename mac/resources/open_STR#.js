82fe8b9aa72ee09844d5b58cd84f799f83f61295
radishengine <radishengine@gmail.com>
2016-09-05 15:24:04 +0100

    Update STR# to new loader format

--- mac/resources/open_STR#.js
+++ mac/resources/open_STR#.js
@@ -2,20 +2,21 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    var strcount = new DataView(resource.data.buffer, resource.data.byteOffset, 2).getInt16(0, false);
-    if (strcount < 0) {
-      console.log(strcount, resource.data);
-      return;
-    }
-    var list = [];
-    var pos = 2;
-    for (var istr = 0; istr < strcount; istr++) {
-      var len = resource.data[pos++];
-      list.push(macintoshRoman(resource.data, pos, len));
-      pos += len;
-    }
-    resource.dataObject = list;
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var strcount = new DataView(bytes.buffer, bytes.byteOffset, 2).getInt16(0, false);
+      if (strcount < 0) {
+        return Promise.reject('invalid string count for STR#');
+      }
+      var list = [];
+      var pos = 2;
+      for (var istr = 0; istr < strcount; istr++) {
+        var len = bytes[pos++];
+        list.push(macintoshRoman(bytes, pos, len));
+        pos += len;
+      }
+      resource.setDataObject(list);
+    });
   };
 
 });
