aab1277f14ad244f79c6564dd80b9bb029e58ac5
radishengine <radishengine@gmail.com>
2016-09-05 14:55:47 +0100

    Update open_BITD.js

--- mac/resources/open_BITD.js
+++ mac/resources/open_BITD.js
@@ -3,7 +3,7 @@ define(['mac/bitpacking'], function(bitpacking) {
   'use strict';
   
   return function(item) {
-    return this.getBytes().then(function(bytes) {
+    return item.getBytes().then(function(bytes) {
       item.getUnpackedData = function() {
         return bitpacking.unpackBits(bytes.buffer, bytes.byteOffset, bytes.byteLength);
       };
