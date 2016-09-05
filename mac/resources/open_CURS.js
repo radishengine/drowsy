e00a63db561c9a8c3c24f82ab96d457e40a54588
radishengine <radishengine@gmail.com>
2016-09-05 17:12:41 +0100

    Resource should be item

--- mac/resources/open_CURS.js
+++ mac/resources/open_CURS.js
@@ -36,7 +36,7 @@ define(['mac/palette2'], function(palette) {
         });
       }
       var hotspotDV = new DataView(bytes.buffer, bytes.byteOffset + (hasMask ? 64 : 32), 4);
-      resource.setHotspot(hotspotDV.getInt16(2), hotspotDV.getInt16(0));
+      item.setHotspot(hotspotDV.getInt16(2), hotspotDV.getInt16(0));
     });
   };
 
