f48a440dcd8b25b02d58fdb7e069e1f99d147fff
radishengine <radishengine@gmail.com>
2016-09-05 15:25:21 +0100

    Update STXT to new loader format

--- mac/resources/open_STXT.js
+++ mac/resources/open_STXT.js
@@ -2,12 +2,14 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    var dataDV = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    var textPos = dataDV.getUint32(0, false);
-    var textLen = dataDV.getUint32(4, false);
-    var num3 = dataDV.getUint32(8, false); // TODO: what is this
-    resource.text = macintoshRoman(resource.data, textPos, textLen);
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dataDV = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var textPos = dataDV.getUint32(0, false);
+      var textLen = dataDV.getUint32(4, false);
+      var num3 = dataDV.getUint32(8, false); // TODO: what is this
+      item.text = macintoshRoman(bytes, textPos, textLen);
+    });
   };
 
 });
