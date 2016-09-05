ecc233eabfece34fc4098c421511722133905528
radishengine <radishengine@gmail.com>
2016-09-05 15:54:15 +0100

    Update vers to new loader format

--- mac/resources/open_vers.js
+++ mac/resources/open_vers.js
@@ -2,25 +2,28 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    resource.dataObject = {
-      major: resource.data[0],
-      minor: resource.data[1],
-      developmentStage: (function(v) {
-        switch(v) {
-          case 0x20: return 'development';
-          case 0x40: return 'alpha';
-          case 0x60: return 'beta';
-          case 0x80: return 'release';
-          default: return v;
-        }
-      })(resource.data[2]),
-      prereleaseRevisionLevel: resource.data[3],
-      regionCode: (resource.data[4] << 8) | resource.data[5],
-    };
-    resource.dataObject.versionNumber = macintoshRoman(resource.data, 7, resource.data[6]);
-    var pos = 7 + resource.data[6];
-    resource.dataObject.versionMessage = macintoshRoman(resource.data, pos + 1, resource.data[pos]);
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dataObject = {
+        major: bytes[0],
+        minor: bytes[1],
+        developmentStage: (function(v) {
+          switch(v) {
+            case 0x20: return 'development';
+            case 0x40: return 'alpha';
+            case 0x60: return 'beta';
+            case 0x80: return 'release';
+            default: return v;
+          }
+        })(bytes[2]),
+        prereleaseRevisionLevel: bytes[3],
+        regionCode: (bytes[4] << 8) | bytes[5],
+      };
+      dataObject.versionNumber = macintoshRoman(bytes, 7, bytes[6]);
+      var pos = 7 + bytes[6];
+      dataObject.versionMessage = macintoshRoman(bytes, pos + 1, bytes[pos]);
+      item.setDataObject(dataObject);
+    });
   };
 
 });
