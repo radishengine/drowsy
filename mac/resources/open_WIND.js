a3ae230b5354db67f3ac03f9d926e7e0f4a4ca80
radishengine <radishengine@gmail.com>
2016-09-05 15:28:08 +0100

    Update WIND to new loader format

--- mac/resources/open_WIND.js
+++ mac/resources/open_WIND.js
@@ -2,25 +2,28 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    var dataDV = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    resource.dataObject = {
-      initialRectangle: {
-        top: dataDV.getInt16(0, false),
-        left: dataDV.getInt16(2, false),
-        bottom: dataDV.getInt16(4, false),
-        right: dataDV.getInt16(6, false),
-      },
-      definitionID: dataDV.getInt16(8, false),
-      visible: dataDV.getInt16(10, false),
-      closeBox: dataDV.getInt16(12, false),
-      referenceConstant: dataDV.getInt32(14, false),
-    };
-    resource.dataObject.title = macintoshRoman(resource.data, 19, resource.data[18]);
-    var pos = 19 + resource.data[18];
-    if (pos+2 <= resource.data.length) {
-      resource.dataObject.positioning = dataDV.getInt16(pos);
-    }
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dataDV = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var dataObject = {
+        initialRectangle: {
+          top: dataDV.getInt16(0, false),
+          left: dataDV.getInt16(2, false),
+          bottom: dataDV.getInt16(4, false),
+          right: dataDV.getInt16(6, false),
+        },
+        definitionID: dataDV.getInt16(8, false),
+        visible: dataDV.getInt16(10, false),
+        closeBox: dataDV.getInt16(12, false),
+        referenceConstant: dataDV.getInt32(14, false),
+      };
+      dataObject.title = macintoshRoman(bytes, 19, bytes[18]);
+      var pos = 19 + bytes[18];
+      if (pos+2 <= bytes.length) {
+        dataObject.positioning = dataDV.getInt16(pos);
+      }
+      item.setDataObject(dataObject);
+    });
   };
 
 });
