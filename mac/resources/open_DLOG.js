d96eb58151681ac97a20fac5b4c666d2880b0a99
radishengine <radishengine@gmail.com>
2016-09-05 15:03:49 +0100

    Change DLOG to new loader format

--- mac/resources/open_DLOG.js
+++ mac/resources/open_DLOG.js
@@ -2,31 +2,34 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    resource.dataObject = {
-      rectangle: {
-        top: dv.getInt16(0, false),
-        left: dv.getInt16(2, false),
-        bottom: dv.getInt16(4, false),
-        right: dv.getInt16(6, false),
-      },
-      type: dv.getUint16(8, false),
-      visible: !!resource.data[10],
-      closeBox: !!resource.data[12],
-      referenceConstant: dv.getInt32(14, false),
-      itemListResourceID: dv.getInt16(18, false),
-    };
-    switch(resource.dataObject.type) {
-      case 0: resource.dataObject.type = 'modal'; break;
-      case 4: resource.dataObject.type = 'modeless'; break;
-      case 5: resource.dataObject.type = 'movableModal'; break;
-    }
-    resource.dataObject.text = macintoshRoman(resource.data, 21, resource.data[20]);
-    var pos = 20 + 1 + resource.dataObject.text.length + (1 + resource.dataObject.text.length) % 2;
-    if (pos + 2 <= resource.data.length) {
-      resource.dataObject.positionCode = dv.getUint16(pos, false);
-    }
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var dataObject = {
+        rectangle: {
+          top: dv.getInt16(0, false),
+          left: dv.getInt16(2, false),
+          bottom: dv.getInt16(4, false),
+          right: dv.getInt16(6, false),
+        },
+        type: dv.getUint16(8, false),
+        visible: !!bytes[10],
+        closeBox: !!bytes[12],
+        referenceConstant: dv.getInt32(14, false),
+        itemListResourceID: dv.getInt16(18, false),
+      };
+      switch(dataObject.type) {
+        case 0: dataObject.type = 'modal'; break;
+        case 4: dataObject.type = 'modeless'; break;
+        case 5: dataObject.type = 'movableModal'; break;
+      }
+      dataObject.text = macintoshRoman(bytes, 21, bytes[20]);
+      var pos = 20 + 1 + dataObject.text.length + (1 + dataObject.text.length) % 2;
+      if (pos + 2 <= bytes.length) {
+        dataObject.positionCode = dv.getUint16(pos, false);
+      }
+      item.setDataObject(dataObject);
+    });
   };
 
 });
