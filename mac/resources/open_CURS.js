f0a2826512345aa8be85653eccb9853e32b8d6e9
radishengine <radishengine@gmail.com>
2016-09-05 14:54:04 +0100

    Change CURS to new loader format

--- mac/resources/open_CURS.js
+++ mac/resources/open_CURS.js
@@ -2,43 +2,41 @@ define(['mac/palette2'], function(palette) {
 
   'use strict';
   
-  return function(resource) {
-    var hasMask;
-    switch(resource.data.length) {
-      case 68: hasMask = true; break;
-      case 37: hasMask = false; break;
-      default:
-        console.error('unexpected length for CURS resource: ' + resource.data.length);
-        return;
-    }
-    var img = document.createElement('CANVAS');
-    img.width = 16;
-    img.height = 16;
-    var ctx = img.getContext('2d');
-    var pix = ctx.createImageData(16, 16);
-    if (hasMask) {
-      for (var ibyte = 0; ibyte < 32; ibyte++) {
-        var databyte = resource.data[ibyte], maskbyte = resource.data[32 + ibyte];
-        for (var ibit = 0; ibit < 8; ibit++) {
-          var imask = 0x80 >> ibit;
-          if (maskbyte & imask) {
-            pix.data.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var hasMask;
+      switch(bytes.length) {
+        case 68: hasMask = true; break;
+        case 37: hasMask = false; break;
+        default:
+          return Promise.reject('unexpected length for CURS resource: ' + bytes.length);
+      }
+      if (hasMask) {
+        item.withPixels(16, 16, function(pixelData) {
+          for (var ibyte = 0; ibyte < 32; ibyte++) {
+            var databyte = bytes[ibyte], maskbyte = bytes[32 + ibyte];
+            for (var ibit = 0; ibit < 8; ibit++) {
+              var imask = 0x80 >> ibit;
+              if (maskbyte & imask) {
+                pixelData.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
+              }
+            }
           }
-        }
+        });
       }
-    }
-    else {
-      for (var ibyte = 0; ibyte < 32; ibyte++) {
-        var databyte = resource.data[ibyte];
-        for (var ibit = 0; ibit < 8; ibit++) {
-          pix.data.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
-        }
+      else {
+        item.withPixels(16, 16, function(pixelData) {
+          for (var ibyte = 0; ibyte < 32; ibyte++) {
+            var databyte = bytes[ibyte];
+            for (var ibit = 0; ibit < 8; ibit++) {
+              pixelData.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
+            }
+          }
+        });
       }
-    }
-    ctx.putImageData(pix, 0, 0);
-    resource.image = {url: img.toDataURL(), width:16, height:16};
-    var hotspotDV = new DataView(resource.data.buffer, resource.data.byteOffset + (hasMask ? 64 : 32), 8);
-    resource.hotspot = {y:hotspotDV.getInt16(0), x:hotspotDV.getInt16(2)};
+      var hotspotDV = new DataView(bytes.buffer, bytes.byteOffset + (hasMask ? 64 : 32), 8);
+      resource.setHotspot(hotspotDV.getInt16(2), hotspotDV.getInt16(0));
+    });
   };
 
 });
