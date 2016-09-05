5a7167bb05cf709045a6dca80f18bb964a9ee877
radishengine <radishengine@gmail.com>
2016-09-05 15:04:57 +0100

    Change FREF to new loader format

--- mac/resources/open_FREF.js
+++ mac/resources/open_FREF.js
@@ -2,15 +2,16 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    if (resource.data.length !== 7) {
-      console.error('FREF resource expected to be 7 bytes, got ' + resource.data.length);
-      return;
-    }
-    resource.dataObject = {
-      fileType: macintoshRoman(resource.data, 0, 4),
-      iconListID: (resource.data[4] << 8) | resource.data[5],
-    };
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      if (bytes.length !== 7) {
+        return Promise.reject('FREF resource expected to be 7 bytes, got ' + bytes.length);
+      }
+      item.setDataObject({
+        fileType: macintoshRoman(resource.data, 0, 4),
+        iconListID: (resource.data[4] << 8) | resource.data[5],
+      });
+    });
   };
 
 });
