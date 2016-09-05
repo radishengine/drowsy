0983861e3e922fc30123b79c2699a1421e95e8de
radishengine <radishengine@gmail.com>
2016-09-05 15:06:59 +0100

    Change ICN# to new loader format

--- mac/resources/open_ICN#.js
+++ mac/resources/open_ICN#.js
@@ -2,27 +2,23 @@ define(['mac/palette2'], function(palette) {
 
   'use strict';
   
-  return function(resource) {
-    if (resource.data.length !== 256) {
-      console.error('ICN# resource expected to be 256 bytes, got ' + resource.data.length);
-      return;
-    }
-    var img = document.createElement('CANVAS');
-    img.width = 32;
-    img.height = 32;
-    var ctx = img.getContext('2d');
-    var pix = ctx.createImageData(32, 32);
-    for (var ibyte = 0; ibyte < 128; ibyte++) {
-      var databyte = resource.data[ibyte], maskbyte = resource.data[128 + ibyte];
-      for (var ibit = 0; ibit < 8; ibit++) {
-        var imask = 0x80 >> ibit;
-        if (maskbyte & imask) {
-          pix.data.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
-        }
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      if (bytes.length !== 256) {
+        return Promise.reject('ICN# resource expected to be 256 bytes, got ' + bytes.length);
       }
-    }
-    ctx.putImageData(pix, 0, 0);
-    resource.image = {url: img.toDataURL(), width:32, height:32};
+      item.withPixels(32, 32, function(pixelData) {
+        for (var ibyte = 0; ibyte < 128; ibyte++) {
+          var databyte = bytes[ibyte], maskbyte = bytes[128 + ibyte];
+          for (var ibit = 0; ibit < 8; ibit++) {
+            var imask = 0x80 >> ibit;
+            if (maskbyte & imask) {
+              pixelData.set(palette[databyte & imask ? 1 : 0], (ibyte*8 + ibit) * 4);
+            }
+          }
+        }
+      });
+    });
   };
 
 });
