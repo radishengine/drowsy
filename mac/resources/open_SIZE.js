d7fc8cc70b5cc222accc472d6c529686431ca8ac
radishengine <radishengine@gmail.com>
2016-09-05 15:22:03 +0100

    Change SIZE to new loader format

--- mac/resources/open_SIZE.js
+++ mac/resources/open_SIZE.js
@@ -2,17 +2,18 @@ define(function() {
 
   'use strict';
   
-  return function(resource) {
-    if (resource.data.length !== 10) {
-      console.error('SIZE: expected length 10, got ' + resource.data.length);
-      return;
-    }
-    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    resource.dataObject = {
-      flags: dv.getUint16(0, false),
-      preferredMemorySize: dv.getUint32(2, false),
-      minimumMemorySize: dv.getUint32(6, false),
-    };
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      if (bytes.data.length !== 10) {
+        return Promise.reject('SIZE: expected length 10, got ' + bytes.length);
+      }
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      item.setDataObject({
+        flags: dv.getUint16(0, false),
+        preferredMemorySize: dv.getUint32(2, false),
+        minimumMemorySize: dv.getUint32(6, false),
+      });
+    });
   }
 
 });
