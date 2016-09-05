8ef4f9968eecd08911c8352f6620d0eba99ca190
radishengine <radishengine@gmail.com>
2016-09-05 15:12:21 +0100

    Change MENU to new loader format

--- mac/resources/open_MENU.js
+++ mac/resources/open_MENU.js
@@ -2,35 +2,38 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(resource) {
-    var dv = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
-    resource.dataObject = {
-      id: dv.getUint16(0, false),
-      definitionProcedureResourceID: dv.getUint16(6, false),
-      enabledState: dv.getUint32(10, false),
-      title: macintoshRoman(resource.data, 15, resource.data[14]),
-    };
-    var pos = 15 + resource.data[14];
-    if (resource.dataObject.definitionProcedureResourceID === 0) {
-      delete resource.dataObject.definitionProcedureResourceID;
-      resource.dataObject.items = [];
-      while (pos < resource.data.length && resource.data[pos] !== 0) {
-        var text = macintoshRoman(resource.data, pos + 1, resource.data[pos]);
-        pos += 1 + text.length;
-        var item = {
-          text: text,
-          iconNumberOrScriptCode: resource.data[pos],
-          keyboardEquivalent: resource.data[pos + 1],
-          markingCharacterOrSubmenuID: resource.data[pos + 2],
-          style: resource.data[pos + 3],
-        };
-        resource.dataObject.items.push(item);
-        pos += 4;
+  return function(item) {
+    return item.getBytes().then(function(bytes) {
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var dataObject = {
+        id: dv.getUint16(0, false),
+        definitionProcedureResourceID: dv.getUint16(6, false),
+        enabledState: dv.getUint32(10, false),
+        title: macintoshRoman(bytes, 15, bytes[14]),
+      };
+      var pos = 15 + bytes[14];
+      if (dataObject.definitionProcedureResourceID === 0) {
+        delete dataObject.definitionProcedureResourceID;
+        dataObject.items = [];
+        while (pos < bytes.length && bytes[pos] !== 0) {
+          var text = macintoshRoman(bytes, pos + 1, bytes[pos]);
+          pos += 1 + text.length;
+          var item = {
+            text: text,
+            iconNumberOrScriptCode: bytes[pos],
+            keyboardEquivalent: bytes[pos + 1],
+            markingCharacterOrSubmenuID: bytes[pos + 2],
+            style: bytes[pos + 3],
+          };
+          dataObject.items.push(item);
+          pos += 4;
+        }
       }
-    }
-    else {
-      resource.dataObject.itemData = atob(String.fromCharCode.apply(null, resource.data.subarray(pos)));
-    }
+      else {
+        dataObject.itemData = atob(String.fromCharCode.apply(null, bytes.subarray(pos)));
+      }
+      item.setDataObject(dataObject);
+    });
   };
 
 });
