8165199b4062ac622ab50f44981519125185d46b
radishengine <radishengine@gmail.com>
2016-09-06 23:09:24 +0100

    Delete 'open_STR ', it is now covered by open_STR_

--- mac/resources/open_STR .js
+++
@@ -1,9 +0,0 @@
-define(['mac/roman'], function(macintoshRoman) {
-
-  return function(item) {
-    return item.getBytes().then(function(bytes) {
-      item.text = macintoshRoman(bytes, 1, bytes[0]);
-    });
-  };
-
-});
