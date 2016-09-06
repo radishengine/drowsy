9d7963c5a27181b4741e33bcbf7f5f0ea14ffdd9
radishengine <radishengine@gmail.com>
2016-09-06 17:39:09 +0100

    Attempt to use region names instead of numbers

--- mac/resources/open_vers.js
+++ mac/resources/open_vers.js
@@ -2,6 +2,54 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
+  function getRegionName(code) {
+    switch(code) {
+      case 0: return 'US';
+      case 1: return 'FR';
+      case 2: return 'GB';
+      case 3: return 'DE';
+      case 4: return 'IT';
+      case 5: return 'NL';
+      case 6: return 'BE/LU';
+      case 7: return 'SE';
+      case 8: return 'ES';
+      case 9: return 'DK';
+      case 10: return 'PT';
+      case 11: return 'fr-CA';
+      case 12: return 'NO';
+      case 13: return 'IL';
+      case 14: return 'JP';
+      case 15: return 'AU';
+      case 16: return 'ar';
+      case 17: return 'FI';
+      case 18: return 'fr-CH';
+      case 19: return 'de-CH';
+      case 20: return 'GR';
+      case 21: return 'IS';
+      case 22: return 'MT';
+      case 23: return 'CY';
+      case 24: return 'TR';
+      case 25: return 'hr-BA';
+      case 33: return 'hi-IN';
+      case 34: return 'ur-PK';
+      case 41: return 'LT';
+      case 42: return 'PL';
+      case 43: return 'HU';
+      case 44: return 'EE';
+      case 45: return 'LV';
+      case 46: return 'FI-10'; // Lapland
+      case 47: return 'FO';
+      case 48: return 'IR';
+      case 49: return 'RU';
+      case 50: return 'IE';
+      case 51: return 'KR';
+      case 52: return 'CN';
+      case 53: return 'TW';
+      case 54: return 'TH';
+      default: return code;
+    }
+  }
+  
   return function(item) {
     return item.getBytes().then(function(bytes) {
       var dataObject = {
@@ -17,7 +65,7 @@ define(['mac/roman'], function(macintoshRoman) {
           }
         })(bytes[2]),
         prereleaseRevisionLevel: bytes[3],
-        regionCode: (bytes[4] << 8) | bytes[5],
+        region: getRegionName((bytes[4] << 8) | bytes[5]),
       };
       dataObject.versionNumber = macintoshRoman(bytes, 7, bytes[6]);
       var pos = 7 + bytes[6];
