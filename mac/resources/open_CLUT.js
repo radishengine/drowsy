cba561d7bd60008333767c6ef1814f053d4636bf
radishengine <radishengine@gmail.com>
2016-09-06 23:30:19 +0100

    resource.data should be bytes

--- mac/resources/open_CLUT.js
+++ mac/resources/open_CLUT.js
@@ -4,11 +4,11 @@ define(function() {
   
   return function(item) {
     item.getBytes().then(function(bytes) {
-      var clut = new DataView(resource.data.buffer, resource.data.byteOffset, resource.data.byteLength);
+      var clut = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
       var seed = clut.getInt32(0, false); // resource ID
       var flags = clut.getUint16(4, false); // 0x8000: color map for indexed device
       if (flags !== 0x0000) {
-        console.log(resource.type, resource.name, resource.data);
+        console.log(resource.type, resource.name, bytes);
         return;
       }
       var entryCount = clut.getUint16(6, false) + 1;
@@ -16,9 +16,9 @@ define(function() {
         for (var icolor = 0; icolor < entryCount; icolor++) {
           var offset = clut.getInt16(8 + icolor*8, false) * 4;
           if (offset >= 0) {
-            pixelData[offset] = resource.data[8 + icolor*8 + 2];
-            pixelData[offset + 1] = resource.data[8 + icolor*8 + 4];
-            pixelData[offset + 2] = resource.data[8 + icolor*8 + 6];
+            pixelData[offset] = bytes[8 + icolor*8 + 2];
+            pixelData[offset + 1] = bytes[8 + icolor*8 + 4];
+            pixelData[offset + 2] = bytes[8 + icolor*8 + 6];
             pixelData[offset + 3] = 255;
           }
         }
