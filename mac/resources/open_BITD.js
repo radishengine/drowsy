d6b9cc9dfdef0a9991dbc8f0d0a015781515f229
radishengine <radishengine@gmail.com>
2016-09-07 18:09:19 +0100

    Update open_BITD.js

--- mac/resources/open_BITD.js
+++ mac/resources/open_BITD.js
@@ -3,16 +3,11 @@ define(['ByteSource', 'itemObjectModel', 'mac/bitpacking'], function(ByteSource,
   'use strict';
   
   function open(item) {
-    var unpackedItem = itemObjectModel.createItem('unpacked');
-    function onPopulateUnpacked() {
-      unpackedItem.removeEventListener(itemObjectModel.EVT_POPULATE, onPopulateUnpacked);
-      unpackedItem.notifyPopulating(item.getBytes().then(function(bytes) {
-        unpackedItem.byteSource = ByteSource.from(bitpacking.unpackBits(bytes.buffer, bytes.byteOffset, bytes.byteLength));
-      }));
-    }
-    unpackedItem.addEventListener(itemObjectModel.EVT_POPULATE, onPopulateUnpacked);
-    item.addItem(unpackedItem);
-    return Promise.resolve(item);
+    return item.getBytes().then(function(bytes) {
+      var unpackedItem = itemObjectModel.createItem('unpacked');
+      unpackedItem.byteSource = ByteSource.from(bitpacking.unpackBits(bytes.buffer, bytes.byteOffset, bytes.byteLength));
+      item.addItem(unpackedItem);
+    });
   }
   
   return open;
