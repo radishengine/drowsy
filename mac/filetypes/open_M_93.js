45ae91d0076ff886ec1afb9a2e2790257423bdd6
radishengine <radishengine@gmail.com>
2016-09-07 12:39:30 +0100

    Handle mmap chunks

--- mac/filetypes/open_M_93.js
+++ mac/filetypes/open_M_93.js
@@ -14,10 +14,17 @@ define(['itemObjectModel'], function(itemObjectModel) {
       }
       item.notifyPopulating(new Promise(function(resolve, reject) {
         for (var pos = 12; pos < bytes.length; ) {
-          var chunkItem = itemObjectModel.createItem(String.fromCharCode.apply(null, bytes.subarray(pos, pos+4)));
+          var chunkName = String.fromCharCode.apply(null, bytes.subarray(pos, pos+4));
+          var chunkItem = itemObjectModel.createItem(chunkName);
           var chunkLen = dv.getUint32(pos + 4);
           if (chunkLen !== 0) {
             chunkItem.byteSource = item.byteSource.slice(pos + 8, pos + 8 + dv.getUint32(pos + 4));
+          }
+          switch (chunkName) {
+            case 'mmap':
+              chunkItem.startAddingItems();
+              chunkItem.addEventListener(itemObjectModel.EVT_POPULATE, MMapView.itemPopulator);
+              break;
           }
           item.addItem(chunkItem);
           pos += 8 + chunkLen + chunkLen % 2;
@@ -27,6 +34,99 @@ define(['itemObjectModel'], function(itemObjectModel) {
 
     });
   }
+  
+  function MMapView(buffer, byteOffset, byteLength) {
+    Object.defineProperties(this, {
+      dataView: {value:new DataView(buffer, byteOffset, byteLength)},
+    });
+  }
+  MMapView.itemPopulator = function() {
+    var self = this;
+    this.notifyPopulating(this.getBytes().then(function(bytes) {
+      var mmap = new MMapView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      self.setDataObject(mmap);
+    }));
+  };
+  MMapView.prototype = {
+    toJSON: function() {
+      return this.entries;
+    },
+  };
+  Object.defineProperties(MMapView.prototype, {
+    entryCount: {
+      get: function() {
+        return this.dv.getUint32(0, false);
+      },
+    },
+    entries: {
+      get: function() {
+        var entries = new Array(this.entryCount);
+        var buffer = this.dataView.buffer, byteOffset = this.dataView.byteOffset;
+        for (var i = 0; i < entries.length; i++) {
+          entries.push(new MMapRecordView(
+            buffer,
+            byteOffset + (i + 1) * MmapRecordView.byteLength,
+            MmapRecordView.byteLength));
+        }
+        return entries;
+      },
+    },
+  });
+  
+  function MMapRecordView(buffer, byteOffset, byteLength) {
+    Object.defineProperties(this, {
+      dataView: {value:new DataView(buffer, byteOffset, byteLength)},
+      bytes: {value:new Uint8Array(buffer, byteOffset, byteLength)},
+    });
+  }
+  MMapRecordView.prototype = {
+    toJSON: function() {
+      if (this.isUnused) return null;
+      return {
+        unknown1: this.unknown1,
+        chunkName: this.chunkName,
+        chunkLength: this.chunkLength,
+        chunkOffset: this.chunkOffset,
+        unknown2: this.unknown2,
+      };
+    },
+  };
+  Object.defineProperties(MMapRecordView.prototype, {
+    unknown1: {
+      get: function() {
+        return this.dataView.getUint32(0, false);
+      },
+    },
+    chunkName: {
+      get {
+        return String.fromCharCode.apply(null, this.bytes.subarray(4, 8));
+      },
+    },
+    isUnused: {
+      get {
+        switch (this.chunkName) {
+          case 'free': case 'junk': return true;
+          default: return false;
+        }
+      },
+    },
+    chunkLength: {
+      get {
+        return this.dataView.getUint32(8, false);
+      },
+    },
+    chunkOffset: {
+      get {
+        return this.dataView.getUint32(12, false);
+      },
+    },
+    unknown2: {
+      get: {
+        return this.dataView.getUint32(16, false);
+      },
+    },
+  });
+  MMapRecordView.byteLength = 20;
   
   return open;
 
