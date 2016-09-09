b37858d428e96dece4a4f6b5f7ccab05b926a3a1
radishengine <radishengine@gmail.com>
2016-09-09 13:17:45 +0100

    Create open_TDTA.js

---
+++ mac/resources/open_TDTA.js
@@ -0,0 +1,27 @@
+define(function() {
+
+  'use strict';
+  
+  function open(item) {
+    return item.getBytes().then(function(bytes) {
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      var list = new Array(dv.getUint16(0, false));
+      var pos = 2;
+      for (var i = 0; i < list.length; i++) {
+        list[i] = new Array(dv.getUint16(pos, false));
+        pos += 2;
+        for (var j = 0; j < sublist.length; j++) {
+          list[i][j] = {
+            unknown_0x00: bytes[pos],
+            unknown_0x01: dv.getInt32(pos) << 2 >> 2, // 24-bit signed integer
+          };
+          pos += 4;
+        }
+      }
+      item.setDataObject(list);
+    });
+  }
+  
+  return open;
+
+});
