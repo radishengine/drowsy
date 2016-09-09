2fb03ad7439146d7153c3afb01ff400995a78a9a
radishengine <radishengine@gmail.com>
2016-09-09 13:29:17 +0100

    Update open_STR_.js

--- mac/resources/open_STR_.js
+++ mac/resources/open_STR_.js
@@ -3,27 +3,27 @@ define(['mac/roman'], function(macintoshRoman) {
   'use strict';
   
   return function(item, typeName) {
-    if (typeName === 'STR ') {
-      // single string
+    if (typeName === 'STR#') {
+      // string table
       return item.getBytes().then(function(bytes) {
-        item.text = macintoshRoman(bytes, 1, bytes[0]);
-      });      
+        var strcount = new DataView(bytes.buffer, bytes.byteOffset, 2).getInt16(0, false);
+        if (strcount < 0) {
+          return Promise.reject('invalid string count for STR#');
+        }
+        var list = [];
+        var pos = 2;
+        for (var istr = 0; istr < strcount; istr++) {
+          var len = bytes[pos++];
+          list.push(macintoshRoman(bytes, pos, len));
+          pos += len;
+        }
+        item.setDataObject(list);
+      });
     }
-    // string table
+    // single string
     return item.getBytes().then(function(bytes) {
-      var strcount = new DataView(bytes.buffer, bytes.byteOffset, 2).getInt16(0, false);
-      if (strcount < 0) {
-        return Promise.reject('invalid string count for STR#');
-      }
-      var list = [];
-      var pos = 2;
-      for (var istr = 0; istr < strcount; istr++) {
-        var len = bytes[pos++];
-        list.push(macintoshRoman(bytes, pos, len));
-        pos += len;
-      }
-      item.setDataObject(list);
-    });
+      item.text = macintoshRoman(bytes, 1, bytes[0]);
+    });      
   };
 
 });
