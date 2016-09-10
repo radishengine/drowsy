0346991fc1dde086628cda9409212dcc54d4f25f
radishengine <radishengine@gmail.com>
2016-09-10 11:29:46 +0100

    Check length of Tdta

--- mac/resources/open_TDTA.js
+++ mac/resources/open_TDTA.js
@@ -5,7 +5,11 @@ define(function() {
   function open(item) {
     return item.getBytes().then(function(bytes) {
       var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
-      var list = new Array(dv.getUint16(0, false));
+      var listLength = dv.getInt16(0, false);
+      if (listLength < 0) {
+        return Promise.reject('TDTA: invalid length');
+      }
+      var list = new Array(listLength);
       var pos = 2;
       for (var i = 0; i < list.length; i++) {
         list[i] = new Array(dv.getUint16(pos, false));
