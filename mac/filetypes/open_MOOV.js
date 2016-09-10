64f0981bce3053879994142b42a3b167d23f71ae
radishengine <radishengine@gmail.com>
2016-09-10 12:05:03 +0100

    Create open_MOOV.js

---
+++ mac/filetypes/open_MOOV.js
@@ -0,0 +1,25 @@
+define(['itemObjectModel, 'mac/roman'], function(itemOM, macRoman) {
+
+  'use strict';
+  
+  function open(item) {
+    function onAtom(item, byteSource) {
+      return byteSource.slice(0, 8).getBytes().then(function(headerBytes) {
+        var length = new DataView(headerBytes.buffer, headerBytes.byteOffset, 4).getUint32(0, false);
+        var name = macRoman(headerBytes, 4, 4);
+        var atomItem = itemOM.createItem(name);
+        if (length > 8) {
+          atomItem.byteSource = byteSource.slice(8, length);
+        }
+        item.addItem(atomItem);
+        if (byteSource.length > (length + 8)) {
+          return onAtom(atomItem, byteSource.slice(length));
+        }
+      });
+    }
+    return onAtom(item, item.byteSource);
+  }
+  
+  return open;
+
+});
