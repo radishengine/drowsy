60b987adddf4f37295765c2e42f9c298084d586a
radishengine <radishengine@gmail.com>
2016-09-10 13:09:25 +0100

    Add support for mvhd atom

--- mac/filetypes/open_MOOV.js
+++ mac/filetypes/open_MOOV.js
@@ -1,4 +1,4 @@
-define(['itemObjectModel', 'mac/roman'], function(itemOM, macRoman) {
+define(['itemObjectModel', 'mac/roman', 'mac/date', 'mac/fixedPoint'], function(itemOM, macRoman, macDate, fixedPoint) {
 
   'use strict';
   
@@ -18,6 +18,11 @@ define(['itemObjectModel', 'mac/roman'], function(itemOM, macRoman) {
               atomItem.startAddingItems();
               atomItem.notifyPopulating(onAtom(atomItem, atomItem.byteSource));
               break;
+            case 'mhvd':
+              atomItem.notifyPopulating(atomItem.byteSource.getBytes().then(function(bytes) {
+                atomItem.setDataObject(new MovieHeaderView(bytes.buffer, bytes.byteOffset, bytes.byteLength));
+              }));
+              break;
           }
         }
         if (byteSource.byteLength >= (length + 8)) {
@@ -27,6 +32,109 @@ define(['itemObjectModel', 'mac/roman'], function(itemOM, macRoman) {
     }
     return onAtom(item, item.byteSource);
   }
+  
+  function MovieHeaderView(buffer, byteOffset, byteLength) {
+    Object.defineProperties(this, {
+      dataView: {value:new DataView(buffer, byteOffset, byteLength)},
+    });
+  }
+  MovieHeaderView.prototype = {
+    toJSON: function() {
+      return {
+        version: this.version,
+        flags: this.flags,
+        creationTime: this.creationTime,
+        modificationTime: this.modificationTime,
+        timeScale: this.timeScale,
+        duration: this.duration,
+        preferredRate: this.preferredRate,
+        preferredVolume: this.preferredVolume,
+        matrix: this.matrix,
+        previewTime: this.previewTime,
+        previewDuration: this.previewDuration,
+        posterTime: this.posterTime,
+        selectionTime: this.selectionTime,
+        selectionDuration: this.selectionDuration,
+        currentTime: this.currentTime,
+        nextTrackID: this.nextTrackID,
+      };
+    },
+    get version() {
+      return this.dataView.getUint8(0);
+    },
+    get flags() {
+      return this.dataView.getUint32(0) & 0xffffff; // unused?
+    },
+    get creationTime() {
+      return macDate(this.dataView, 4);
+    },
+    get modificationTime() {
+      return macDate(this.dataView, 8);
+    },
+    get timeScale() {
+      return this.dataView.getUint32(12, false); // time units per second
+    },
+    get duration() {
+      return this.dataView.getUint32(16, false); // in time units
+    },
+    get preferredRate() {
+      return fixedPoint.fromInt32(this.dataView.getInt32(20, false)); // 1.0 = normal
+    },
+    get preferredVolume() {
+      return fixedPoint.fromUint16(this.dataView.getUint16(24, false)); // 1.0 = normal
+    },
+    // reserved: 10 bytes
+    get matrix() {
+      var matrix = new MatrixView(this.dataView.buffer, this.dataView.byteOffset + 36, MatrixView.byteLength);
+      Object.defineProperty(this, 'matrix', {value:matrix});
+      return matrix;
+    },
+    get previewTime() {
+      return this.dataView.getUint32(72, false); // in time units
+    },
+    get previewDuration() {
+      return this.dataView.getUint32(76, false); // in time units
+    },
+    get posterTime() {
+      return this.dataView.getUint32(80, false); // in time units
+    },
+    get selectionTime() {
+      return this.dataView.getUint32(84, false); // in time units
+    },
+    get selectionDuration() {
+      return this.dataView.getUint32(88, false); // in time units
+    },
+    get currentTime() {
+      return this.dataView.getUint32(92, false); // in time units
+    },
+    get nextTrackID() {
+      return this.dataView.getUint32(96, false); // zero is not a valid track number
+    },
+  };
+  MovieHeaderView.byteLength = 100;
+  
+  function MatrixView(buffer, byteOffset, byteLength) {
+    this.dataView = new DataView(buffer, byteOffset, byteLength);
+  }
+  MatrixView.prototype = {
+    toJSON: function() {
+      return {
+        a: this.a, b: this.b, u: this.u,
+        c: this.c, d: this.d, v: this.v,
+        x: this.x, y: this.y, w: this.w,
+      };
+    },
+    get a() { return fixedPoint.fromInt32(this.dataView.getInt32(0, false)); },
+    get b() { return fixedPoint.fromInt32(this.dataView.getInt32(4, false)); },
+    get u() { return fixedPoint.fromInt32_2_30(this.dataView.getInt32(8, false)) },
+    get c() { return fixedPoint.fromInt32(this.dataView.getInt32(12, false)); },
+    get d() { return fixedPoint.fromInt32(this.dataView.getInt32(16, false)); },
+    get v() { return fixedPoint.fromInt32_2_30(this.dataView.getInt32(20, false)) },
+    get x() { return fixedPoint.fromInt32(this.dataView.getInt32(24, false)); },
+    get y() { return fixedPoint.fromInt32(this.dataView.getInt32(28, false)) },
+    get w() { return fixedPoint.fromInt32_2_30(this.dataView.getInt32(32, false)) },
+  };
+  MatrixView.byteLength = 4 * 9;
   
   return open;
 
