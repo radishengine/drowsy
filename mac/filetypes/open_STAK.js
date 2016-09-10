98513fa81c5502c5984c0b10fadde1bf35c8815d
radishengine <radishengine@gmail.com>
2016-09-11 00:42:07 +0100

    Create open_STAK.js

---
+++ mac/filetypes/open_STAK.js
@@ -0,0 +1,25 @@
+define(['mac/roman'], function(macRoman) {
+
+  'use strict';
+  
+  function open(item) {
+    function onBlock(item, byteSource) {
+      return byteSource.slice(0, 8).getBytes().then(function(headerBytes) {
+        var length = new DataView(headerBytes.buffer, headerBytes.byteOffset, 4).getUint32(0, false);
+        var name = macRoman(headerBytes, 4, 4);
+        var blockItem = itemOM.createItem(name);
+        item.addItem(blockItem);
+        if (length > 8) {
+          blockItem.byteSource = byteSource.slice(8, length);
+        }
+        if (byteSource.byteLength >= (length + 8)) {
+          return onBlock(item, byteSource.slice(length));
+        }
+      });
+    }
+    return onBlock(item, item.byteSource);
+  }
+  
+  return open;
+
+});
