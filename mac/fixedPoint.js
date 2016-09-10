0157777cc93b00bae38abc684af44cbdc01e1224
radishengine <radishengine@gmail.com>
2016-09-10 12:52:03 +0100

    Add support for 2.30

--- mac/fixedPoint.js
+++ mac/fixedPoint.js
@@ -12,6 +12,15 @@ define(function() {
       }
       return (i32 >>> 16) + frac;
     },
+    fromInt32_2_30: function(i32) {
+      var frac = 0;
+      for (var i = 0; i < 30; i++) {
+        if (i32 & (0x20000000 >> i)) {
+          frac += 1 / (2 << i);
+        }
+      }
+      return (i32 >>> 30) + frac;
+    },
   };
 
 });
