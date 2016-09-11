be73263156f50c803828b16dac844a04913abfd4
radishengine <radishengine@gmail.com>
2016-09-11 01:00:33 +0100

    Use block IDs

--- mac/filetypes/open_STAK.js
+++ mac/filetypes/open_STAK.js
@@ -4,15 +4,17 @@ define(['itemObjectModel', 'mac/roman'], function(itemOM, macRoman) {
   
   function open(item) {
     function onBlock(item, byteSource) {
-      return byteSource.slice(0, 8).getBytes().then(function(headerBytes) {
-        var length = new DataView(headerBytes.buffer, headerBytes.byteOffset, 4).getUint32(0, false);
+      return byteSource.slice(0, 12).getBytes().then(function(headerBytes) {
+        var dv = new DataView(headerBytes.buffer, headerBytes.byteOffset, 4);
+        var length = dv.getUint32(0, false);
         var name = macRoman(headerBytes, 4, 4);
-        var blockItem = itemOM.createItem(name);
+        var id = dv.getInt32(8, false);
+        var blockItem = itemOM.createItem(name + " " + id);
         item.addItem(blockItem);
         if (length > 8) {
-          blockItem.byteSource = byteSource.slice(8, length);
+          blockItem.byteSource = byteSource.slice(12, length);
         }
-        if (byteSource.byteLength >= (length + 8)) {
+        if (byteSource.byteLength >= (length + 12)) {
           return onBlock(item, byteSource.slice(length));
         }
       });
