6cf67e981198a0723204e48fc82006c97d5a8d3d
radishengine <radishengine@gmail.com>
2016-09-11 23:51:10 +0100

    Try casting value to signed byte

--- mac/resources/open_ASND.js
+++ mac/resources/open_ASND.js
@@ -10,9 +10,11 @@ define(function() {
       var value = 0x80;
       for (var i = 0; i < bytes.length; i++) {
         value += deltas[bytes[i] & 0xf];
-        samples[i*2] = (value << 24 >> 24) + 128;
+        value = value << 24 >> 24;
+        samples[i*2] = value & 0xff;
         value += deltas[(bytes[i] >> 4) & 0xf];
-        samples[i*2 + 1] = (value << 24 >> 24) + 128;
+        value = value << 24 >> 24;
+        samples[i*2 + 1] = value & 0xff;
       }
       item.setRawAudio({
         channels: 1,
