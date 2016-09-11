aed2b5068e73b430967aab8edb29393ad9f2a8dc
radishengine <radishengine@gmail.com>
2016-09-11 21:08:07 +0100

    Add text window data fields

--- mac/resources/open_ATXT.js
+++ mac/resources/open_ATXT.js
@@ -2,11 +2,50 @@ define(['mac/roman'], function(macintoshRoman) {
 
   'use strict';
   
-  return function(item) {
+  function open(item) {
     return item.getBytes().then(function(bytes) {
-      var text = macintoshRoman(bytes, 0, bytes.length);
-      debugger;
+      item.setDataObject(new TextView(bytes.buffer, bytes.byteOffset, bytes.byteLength));
     });
   };
+  
+  function TextView(buffer, byteOffset, byteLength) {
+    this.dataView = new DataView(buffer, byteOffset, byteLength);
+  };
+  TextView.prototype = {
+    toJSON: function() {
+      return {
+        top: this.top,
+        left: this.left,
+        bottom: this.bottom,
+        right: this.right,
+        fontType: this.fontType,
+        fontSize: this.fontSize,
+        text: this.text,
+      };
+    },
+    get top() {
+      return this.dataView.getInt16(0, false);
+    },
+    get left() {
+      return this.dataView.getInt16(2, false);
+    },
+    get bottom() {
+      return this.dataView.getInt16(4, false);
+    },
+    get right() {
+      return this.dataView.getInt16(6, false);
+    },
+    get fontType() {
+      return this.dataView.getUint16(8, false);
+    },
+    get fontSize() {
+      return this.dataView.getUint16(10, false);
+    },
+    get text() {
+      return macRoman(this.bytes, 12);
+    },
+  };
+  
+  return open;
 
 });
