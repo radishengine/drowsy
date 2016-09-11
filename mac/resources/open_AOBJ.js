62f78714b4bf3a11bc1959e97550bb520ef8e01a
radishengine <radishengine@gmail.com>
2016-09-11 22:24:48 +0100

    Create open_AOBJ.js

---
+++ mac/resources/open_AOBJ.js
@@ -0,0 +1,53 @@
+define(['mac/roman'], function(macRoman) {
+
+  'use strict';
+  
+  function open(item) {
+    return item.getBytes().then(function(bytes) {
+      var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+      
+      var pos = dv.getUint16(0, false);
+      
+      var obj = {
+        top: this.getInt16(pos, false),
+        left: this.getInt16(pos + 2, false),
+        bottom: this.getInt16(pos + 4, false),
+        right: this.getInt16(pos + 6, false),
+        isPlural: !!bytes[pos + 8],
+        // unknown: 4 bytes
+        accuracy: bytes[pos + 13],
+        value: bytes[pos + 14],
+        type: bytes[pos + 15],
+        damage: bytes[pos + 16],
+        attackType: bytes[pos + 17],
+        numberOfUses: dv.getUint16(pos + 18, false),
+        returnToRandomScene: !!bytes[pos + 20],
+        // unknown: 1 byte
+      };
+      pos += 22;
+      
+      obj.sceneOrOwner = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      obj.clickMessage = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      obj.operativeVerb = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      obj.failureMessage = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      obj.useMessage = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      obj.sound = macRoman(bytes, pos + 1, bytes[pos]);
+      pos += 1 + bytes[pos];
+      
+      item.setDataObject(obj);
+    });
+  }
+  
+  return open;
+
+});
