5e8c40b41ed98576fc4b54425bac2cbbc95fad4e
radishengine <radishengine@gmail.com>
2016-09-11 23:45:27 +0100

    Use supplied deltas instead of hard coding

--- mac/resources/open_ASND.js
+++ mac/resources/open_ASND.js
@@ -2,18 +2,17 @@ define(function() {
 
   'use strict';
   
-  var DELTAS = new Int8Array([0, -49, -36, -25, -16, -9, -4, -1, 0, 1, 4, 9, 16, 25, 36, 49]);
-
   function open(item) {
     return item.getBytes().then(function(bytes) {
+      var deltas = new Int8Array(bytes.buffer, bytes.byteOffset + 4, 16);
       bytes = bytes.subarray(20);
       var samples = new Uint8Array(2 * bytes.length);
       var value = 0x80;
       for (var i = 0; i < bytes.length; i++) {
-        value += DELTAS[bytes[i] & 0xf];
+        value += deltas[bytes[i] & 0xf];
         value &= 0xff;
         samples[i*2] = value;
-        value += DELTAS[(bytes[i] >> 4) & 0xf];
+        value += deltas[(bytes[i] >> 4) & 0xf];
         value &= 0xff;
         samples[i*2 + 1] = value;
       }
