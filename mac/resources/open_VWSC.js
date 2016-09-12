2399aeaf169b9eb4d21b838c2100b51fea20f022
radishengine <radishengine@gmail.com>
2016-09-12 21:40:39 +0100

    Remove debug play screen

--- mac/resources/open_VWSC.js
+++ mac/resources/open_VWSC.js
@@ -14,25 +14,6 @@ define(function() {
           return new FramePlayHead(bytes.buffer, bytes.byteOffset + 4, len - 4);
         },
       };
-      var testScreen = document.createElement('CANVAS');
-      testScreen.width = 640;
-      testScreen.height = 400;
-      var ctx = testScreen.getContext('2d');
-      var playHead = testScreen.playHead = item.playHeadFactory.create();
-      var sprites = playHead.sprites;
-      testScreen.playHead.eventTarget = testScreen;
-      testScreen.addEventListener('enter-frame', function() {
-        ctx.clearRect(0, 0, 640, 480);
-        for (var i = 0; i < sprites.length; i++) {
-          if (!sprites[i].cast) continue;
-          ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255)
-            + ',' + Math.floor(Math.random() * 255)
-            + ',' + Math.floor(Math.random() * 255) + ')';
-          ctx.fillRect(sprites[i].top, sprites[i].left, sprites[i].width, sprites[i].height);
-        }
-      });
-      item.addItem(testScreen);
-      testScreen.playHead.next();
     });
   }
   
