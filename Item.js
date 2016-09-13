180d9ae34c335a3e9e64b4b124bc73a1a143f214
radishengine <radishengine@gmail.com>
2016-09-13 17:02:42 +0100

    Remove item collection again

--- Item.js
+++ Item.js
@@ -11,24 +11,6 @@ define(function() {
       return this.eventTarget.dispatchEvent.apply(this.eventTarget, arguments);
     },
   };
-  
-  function ItemCollection() {
-  }
-  
-  ItemCollection.prototype = {
-    eventTarget: window,
-    dispatchEvent: function() {
-      return this.eventTarget.dispatchEvent.apply(this.eventTarget, arguments);
-    },
-    add: function(item) {
-      this.dispatchEvent(new CustomEvent('item-added', {detail:{collection:this, item:item}}));
-    },
-    markComplete: function() {
-      this.dispatchEvent(new CustomEvent('marked-complete', {detail:{collection:this}}));
-    },
-  };
-
-  Item.Collection = ItemCollection;
 
   return Item;
 
