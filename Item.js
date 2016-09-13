39ad7f4b03822935af342fb411fb1b84d69ca9bf
radishengine <radishengine@gmail.com>
2016-09-13 16:48:01 +0100

    Add ItemCollection

--- Item.js
+++ Item.js
@@ -11,6 +11,21 @@ define(function() {
       return this.eventTarget.dispatchEvent.apply(this.eventTarget, arguments);
     },
   };
+  
+  function ItemCollection() {
+  }
+  
+  ItemCollection.prototype = {
+    eventTarget: window,
+    dispatchEvent: function() {
+      return this.eventTarget.dispatchEvent.apply(this.eventTarget, arguments);
+    },
+    add: function(item) {
+      this.dipatchEvent(new CustomEvent('item-added', {detail:{item:item}}));
+    },
+  };
+
+  Item.Collection = ItemCollection;
 
   return Item;
 
