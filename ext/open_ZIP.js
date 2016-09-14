09bf5d6318346497690697a3eff17f25dfe23c4d
radishengine <radishengine@gmail.com>
2016-09-14 20:12:47 +0100

    Get the central directory records

--- ext/open_ZIP.js
+++ ext/open_ZIP.js
@@ -5,6 +5,29 @@ define(['msdos/util', 'text', 'Item'], function(dosUtil, text, Item) {
   function open() {
     var byteSource = this.byteSource;
     this.addExplorer(function(expedition) {
+      byteSource.slice(-TrailerFixedView.byteLength).getBytes()
+      .then(function(rawTrailer) {
+        var fixedPart = new TrailerFixedView(rawTrailer.buffer, rawTrailer.byteOffset, rawTrailer.byteLength);
+        if (!fixedPart.hasValidSignature) {
+          // TODO: support comments
+          throw new Exception('ZIP files with comments not supported');
+        }
+        if (fixedPart.currentDiskEntryCount !== fixedPart.totalEntryCount) {
+          // TODO: support multi-part ZIP files
+          throw new Exception('multi-part ZIP files not supported');
+        }
+        var pos = fixedPart.centralDirectoryFirstDiskOffset, len = fixedPart.centralDirectoryByteLength;
+        return byteSource.slice(pos, pos + len).getBytes()
+        .then(function(rawCentralDirectory) {
+          var fileRecords = CentralFileRecordView.getList(
+            trailer.totalEntryCount,
+            rawCentralDirectory.buffer,
+            rawCentralDirectory.byteOffset,
+            rawCentralDirectory.byteLength);
+          console.log(fileRecords);
+        });
+      });
+      /*
       function onLocalFileHeader(bytes) {
         var signature = String.fromCharCode.apply(null, bytes.subarray(0, 4));
         switch (signature) {
@@ -48,8 +71,210 @@ define(['msdos/util', 'text', 'Item'], function(dosUtil, text, Item) {
         });
       }
       byteSource.slice(0, LocalFileHeaderFixedView.byteLength).getBytes().then(onLocalFileHeader);
+      */
     });
   }
+  
+  function TrailerFixedView(buffer, byteSource, byteLength) {
+    this.dataView = new DataView(buffer, byteSource, byteLength);
+    this.bytes = new Uint8Array(buffer, byteSource, byteLength);
+  }
+  TrailerFixedView.prototype = {
+    get hasValidSignature() {
+      return String.fromCharCode.apply(null, this.bytes.subarray(0, 4)) === TrailerFixedView.signature;
+    },
+    get diskNumber() {
+      return this.dataView.getUint16(4, true);
+    },
+    get centralDirectoryFirstDiskNumber() {
+      return this.dataView.getUint16(6, true);
+    },
+    get currentDiskEntryCount() {
+      return this.dataView.getUint16(8, true);
+    },
+    get totalEntryCount() {
+      return this.dataView.getUint16(10, true);
+    },
+    get centralDirectoryByteLength() {
+      return this.dataView.getUint32(12, true);
+    },
+    get centralDirectoryFirstDiskOffset() {
+      return this.dataView.getUint32(16, true);
+    },
+    get commentByteLength() {
+      return this.dataView.getUint16(20, true);
+    },
+  };
+  TrailerFixedView.signature = 'PK\x05\x06';
+  TrailerFixedView.byteLength = 22;
+  
+  function CentralFileRecordView(buffer, byteOffset, byteLength) {
+    this.dataView = new DataView(buffer, byteOffset, byteLength);
+    this.bytes = new Uint8Array(buffer, byteOffset, byteLength);
+  }
+  CentralFileRecordView.prototype = {
+    get hasValidSignature() {
+      return String.fromCharCode.apply(null, this.bytes.subarray(0, 4)) === CentralFileRecordView.signature;
+    },
+    get creationSystem() {
+      var systemCode = this.dataView.getUint8(5);
+      switch(systemCode) {
+        case 0: return 'fat'; // FAT / VFAT / FAT32: MS-DOS and OS/2
+        case 1: return 'amiga';
+        case 2: return 'openVMS';
+        case 3: return 'unix';
+        case 4: return 'vm/cms';
+        case 5: return 'atariST';
+        case 6: return 'hpfs'; // OS/2
+        case 7: return 'mac';
+        case 8: return 'z';
+        case 9: return 'cp/m';
+        case 10: return 'ntfs';
+        case 11: return 'mvs';
+        case 12: return 'vse';
+        case 13: return 'acorn';
+        case 14: return 'vfat';
+        case 15: return 'mvsAlternate';
+        case 16: return 'beOS';
+        case 17: return 'tandem';
+        case 18: return 'os/400';
+        case 19: return 'osx'; // darwin
+        default: return systemCode;
+      }
+    },
+    get zipSpecificationVersion() {
+      return this.dataView.getUint8(4);
+    },
+    get requiredPKZipVersion() {
+      return this.dataView.getUint16(6, true) / 10;
+    },
+    get flags() {
+      return this.dataView.getUint16(8, true);
+    },
+    get isEncrypted() {
+      return !!(this.flags & (1 << 0));
+    },
+    get usesCompressionOption1() {
+      return !!(this.flags & (1 << 1));
+    },
+    get usesCompressionOption2() {
+      return !!(this.flags & (1 << 2));
+    },
+    get hasDataDescriptor() {
+      return !!(this.flags & (1 << 3));
+    },
+    get hasEnhancedDeflation() {
+      return !!(this.flags & (1 << 4));
+    },
+    get hasCompressedPatchedData() {
+      return !!(this.flags & (1 << 5));
+    },
+    get hasStrongEncryption() {
+      return !!(this.flags & (1 << 6));
+    },
+    get hasUTF8Encoding() {
+      return !!(this.flags & (1 << 11));
+    },
+    get hasMaskHeaderValues() {
+      return !!(this.flags & (1 << 13));
+    },
+    get compressionMethod() {
+      var methodCode = this.dataView.getUint16(10, true);
+      switch(methodCode) {
+        case 0: return 'none';
+        case 1: return 'shrunk';
+        case 2: return 'factor1';
+        case 3: return 'factor2';
+        case 4: return 'factor3';
+        case 5: return 'factor4';
+        case 6: return 'imploded';
+        case 8: return 'deflated';
+        case 9: return 'enhancedDeflated';
+        case 10: return 'dclImploded';
+        case 12: return 'bzip2';
+        case 14: return 'lzma';
+        case 18: return 'terse';
+        case 19: return 'lz77';
+        case 98: return 'ppmd';
+        default: return methodCode;
+      }
+    },
+    get modifiedAt() {
+      return dosUtil.getTimeAndDate(this.dataView, 12);
+    },
+    get crc32() {
+      return ('0000000' + this.dataView.getUint32(16, true).toString(16).toUpperCase()).slice(-8);
+    },
+    get compressedSize32() {
+      return this.dataView.getUint32(20, true);
+    },
+    get uncompressedSize32() {
+      return this.dataView.getUint32(24, true);
+    },
+    get isZip64() {
+      return this.compressedSize32 === 0xffffffff && this.uncompressedSize32 === 0xffffffff;
+    },
+    get pathByteLength() {
+      return this.dataView.getUint16(28, true);
+    },
+    get extraByteLength() {
+      return this.dataView.getUint16(30, true);
+    },
+    get commentByteLength() {
+      return this.dataView.getUint16(32, true);
+    },
+    get firstDiskNumber() {
+      return this.dataView.getUint16(34, true);
+    },
+    get internalAttributes() {
+      return this.dataView.getUint16(36, false);
+    },
+    get isApparentlyTextFile() {
+      return !!(this.internalAttributes && (1 << 0));
+    },
+    get hasControlFieldRecordsBeforeLogicalRecords() {
+      return !!(this.internalAttributes && (1 << 2));
+    },
+    get externalAttributes() {
+      return this.dataView.getUint32(38, false);
+    },
+    get localHeaderOffset() {
+      return this.dataView.getUint32(42, false);
+    },
+    get pathPos() {
+      return CentralFileRecordView.fixedByteLength;
+    },
+    get decode() {
+      return this.hasUTF8Encoding() ? text.decodeUTF8 : dosUtil.decodeLatinUS;
+    },
+    get path() {
+      return this.decode(this.bytes, this.pathPos, this.pathByteLength);
+    },
+    get extraPos() {
+      return this.pathPos + this.pathByteLength;
+    },
+    get commentPos() {
+      return this.extraPos + this.extraByteLength;
+    },
+    get comment() {
+      return this.decode(this.bytes, this.commentPos, this.commentByteLength);
+    },
+    get byteLength() {
+      return this.pathPos + this.pathByteLength + this.extraByteLength + this.commentByteLength;
+    },
+  };
+  CentralFileRecordView.signature = 'PK\x01\x02';
+  CentralFileRecordView.fixedByteLength = 46;
+  CentralFileRecordView.getList = function(count, buffer, byteOffset, byteLength) {
+    var list = [];
+    var pos = 0;
+    for (; count > 0; count--) {
+      var record = new CentralFileHeaderFixedView(buffer, byteOffset + pos, byteLength - pos);
+      pos += record.byteLength;
+      list.push(record);
+    }
+    return list;
+  };
   
   function LocalFileHeaderFixedView(buffer, byteOffset, byteLength) {
     this.dataView = new DataView(buffer, byteOffset, byteLength);
