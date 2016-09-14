266245b55abf12fb9a3f7076fbdde8ed83baafde
radishengine <radishengine@gmail.com>
2016-09-14 15:08:04 +0100

    Add system to promise a listing of items found

--- Item.js
+++ Item.js
@@ -4,12 +4,60 @@ define(function() {
   
   function Item(byteSource) {
     this.byteSource = byteSource;
+    this.explorers = [];
   }
   
   Item.prototype = {
     eventTarget: window,
     dispatchEvent: function() {
       return this.eventTarget.dispatchEvent.apply(this.eventTarget, arguments);
+    },
+    addExplorer: function(explorer) {
+      this.explorers.push(explorer);
+    },
+    getListing: function() {
+      var self = this;
+      return new Promise(function(resolve, reject) {
+        var expedition = {
+          explorers: self.explorers.slice(),
+          resolvedCount: 0,
+          listing: [],
+          foundItem: function(item) {
+            this.listing.push(item);
+          },
+          conclude: function() {
+            if (++this.resolvedCount === this.explorers.length) {
+              resolve(this.listing);
+            }
+          },
+          go: function() {
+            if (this.explorers.length === 0) {
+              resolve([]);
+              return;
+            }
+            for (var i = 0; i < this.explorers.count; i++) {
+              this.explorers[i].apply(self, this);
+            }
+          },
+        };
+        expedition.go();
+      });
+      var expedition = {
+        listing: [],
+        foundItem: function(item) {
+          this.listing.push(item);
+        },
+        explorers: this.explorers.slice(),
+        go: function(resolve, reject) {
+          if (this.explorers.length === 0) {
+            return Promise.resolve(this.listing);
+          }
+          for (var i = 0; i < this.explorers.length; i++) {
+            this.explorers[i](this);
+          }
+        },
+      };
+      return new Promise(expedition.go.bind(expedition));
     },
   };
 
