219b29852422a7187eaef6fdd0bab3c1a04d24ae
radishengine <radishengine@gmail.com>
2016-09-15 15:44:20 +0100

    Add swap32

--- z/util.js
+++ z/util.js
@@ -87,6 +87,13 @@ define(function() {
       /* return recombined sums */
       return adler | (sum2 << 16);
     },
+    // reverse the bytes of a 32-bit integer
+    swap32: function(q) {
+      return ((q >> 24) & 0xff)
+        | ((q >> 8) & 0xff00)
+        | ((q & 0xff00) << 8)
+        | ((q & 0xff) << 24);
+    },
   };
 
 });
