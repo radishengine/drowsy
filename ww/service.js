6574b3fbc601f4e3b6cc8f67687dc9299ea9c109
radishengine <radishengine@gmail.com>
2016-09-21 16:56:41 +0100

    importScripts is relative to the script

--- ww/service.js
+++ ww/service.js
@@ -14,7 +14,7 @@ self.onmessage = function(e) {
       var lib = command[1];
       if (!(command in loaded)) {
         try {
-          importScripts('ww/' + lib + '.js');
+          importScripts(lib + '.js');
         }
         catch (e) {
           postMessage(['loadfailed', lib]);
