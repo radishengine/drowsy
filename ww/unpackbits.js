73bd947f8233d8681e06dfa47064c147ab7f7620
radishengine <radishengine@gmail.com>
2016-09-23 18:03:50 +0100

    Move to a switch loop version

--- ww/unpackbits.js
+++ ww/unpackbits.js
@@ -1,37 +1,64 @@
 
-onmessage = function(e) {
-  var command = e.command;
-  var packed = command.compressedBytes;
-  var buf_length = packed.length;
-  var buf_pos = 0;
-  var buf = new Uint8Array(buf);
-  function checkOutputSpace(n) {
-    if (buf_pos + n > buf_length) {
-      buf_length *= 2;
-      var new_buf = new Uint8Array(buf_length);
-      new_buf.set(buf);
-      buf = new_buf;
-    }
-  }
-  var pos = 0;
-  while (pos < packed.length) {
-    var b = packed[pos++];
-    if (b & 0x80) {
-      if (b === 0x80) continue;
-      var count = 257 - b;
-      var rep = packed[pos++];
-      checkOutputSpace(count);
-      for (var i = 0; i < count; i++) {
-        buf[buf_pos++] = rep;
-      }
-    }
-    else {
-      var length = b + 1;
-      checkOutputSpace(length);
-      buf.set(packed.subarray(pos, pos + length), buf_pos);
-      pos += length;
-      buf_pos += length;
-    }
-  }
-  postMessage(buf, [buf.buffer]);
+function BitUnpacker() {
+}
+BitUnpacker.prototype = {
+  mode: 'default',
+  count: 0,
+  context_value: 0,
+  process: function(input, output) {
+    var input_pos = 0, input_end = input.length,
+      output_pos = 0, output_end = output.length,
+      mode = this.mode,
+      count = this.count,
+      context_value = this.context_value;
+    unpacking: do switch (mode) {
+      case 'default':
+        if (input_pos === input_end) break unpacking;
+        count = input[input_pos++];
+        if (count & 0x80) {
+          if (count === 0x80) {
+            // TODO: is this the end marker?
+            continue unpacking;
+          }
+          count = 257 - count;
+          mode = 'repeat';
+          continue unpacking;
+        }
+        mode = 'literal';
+//      continue unpacking;
+      case 'literal':
+        do {
+          if (output_pos === output_end || input_pos === input_end) {
+            this.context_value = context_value;
+            break;
+          }
+          output_pos[output_pos++] = input[input_pos++];
+        } while (--count);
+        mode = 'default';
+        continue unpacking;
+      case 'repeat':
+        if (input_pos === input_end) {
+          this.count = count;
+          break unpacking;
+        }
+        context_value = input[input_pos++];
+        mode = 'repeat2';
+//      continue unpacking;
+      case 'repeat2':
+        do {
+          if (output_pos === output_end) {
+            this.context_value = context_value;
+            break;
+          }
+          output[output_pos++] = context_value;
+        } while (--count);
+        mode = 'default';
+        continue unpacking;
+    } while (true);
+    this.mode = mode;
+  },
+};
+
+self.init_unpackbits = function() {
+  return new BitUnpacker();
 };
