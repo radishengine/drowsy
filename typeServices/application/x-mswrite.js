e228823af8ccbd8b2d73868d8ed4e6a2c6eb0ff2
radishengine <radishengine@gmail.com>
2016-09-24 15:59:34 +0100

    Supply full split function

--- typeServices/application/x-mswrite.js
+++ typeServices/application/x-mswrite.js
@@ -2,12 +2,58 @@ define(function() {
 
   'use strict';
   
+  function split() {
+    var context = this;
+    return context.getBytes(0, FileHeaderView.byteLength).then(function(rawHeader) {
+      var header = new FileHeaderView(rawHeader.buffer, rawHeader.byteOffset, rawHeader.byteLength);
+      if (!header.hasValidSignature) return Promise.reject('not a valid Write file');
+      if (header.isWordFile) return Promise.reject('Word documents not yet supported'); // TODO
+      var promises = [];
+      if (header.textLength > 0) {
+        /* windows-1252 text with binary/OLE stuff mixed in */
+        context.addEntry(context.getSegment(header.textOffset, header.textLength), {
+          type: 'application/x-mswrite-text-data',
+        });
+      }
+      if (header.characterInfoLength > 0) {
+        context.addEntry(context.getSegment(header.characterInfoOffset, header.characterInfoLength), {
+          type: 'application/x-mswrite-character-info',
+        });
+      }
+      if (header.paragraphInfoLength > 0) {
+        context.addEntry(context.getSegment(header.paragraphInfoOffset, header.paragraphInfoLength), {
+          type: 'application/x-mswrite-paragraph-info',
+        });
+      }
+      if (header.footnoteTableLength > 0) {
+        context.addEntry(context.getSegment(header.footnoteTableOffset, header.footnoteTableLength), {
+          type: 'application/x-mswrite-footnote-table',
+        });
+      }
+      if (header.sectionTableLength > 0) {
+        context.addEntry(context.getSegment(header.sectionTableOffset, header.sectionTableLength), {
+          type: 'application/x-mswrite-section-table',
+        });
+      }
+      if (header.pageTableLength > 0) {
+        context.addEntry(context.getSegment(header.pageTableOffset, header.pageTableLength), {
+          type: 'application/x-mswrite-page-table',
+        });
+      }
+      if (header.fontNameTableLength > 0) {
+        context.addEntry(context.getSegment(header.fontNameTableOffset, header.fontNameTableLength), {
+          type: 'application/x-mswrite-font-name-table',
+        });
+      }
+    });
+  }
+  
   var PAGE_BYTES = 128;
   
-  function WriteFileHeaderView(buffer, byteOffset, byteLength) {
+  function FileHeaderView(buffer, byteOffset, byteLength) {
     this.dv = new DataView(buffer, byteOffset, byteLength);
   }
-  WriteFileHeaderView.prototype = {
+  FileHeaderView.prototype = {
     get hasValidSignature() {
       return this.signature === 0xBE31 || this.signature === 0xBE32;
     },
@@ -34,49 +80,49 @@ define(function() {
     get characterInfoOffset() {
       return this.textOffset + PAGE_BYTES * Math.ceil(this.textByteLength / PAGE_BYTES);
     },
-    get characterInfoBlockLength() {
+    get characterInfoLength() {
       return this.paragraphInfoOffset - this.characterInfoOffset;
     },
     get paragraphInfoOffset() {
       return PAGE_BYTES * this.dv.getUint16(18, true);
     },
-    get paragraphInfoBlockLength() {
+    get paragraphInfoLength() {
       return PAGE_BYTES * (this.dv.getUint16(20, true) - this.dv.getUint16(18, true));
     },
     get footnoteTableOffset() {
       return PAGE_BYTES * this.dv.getUint16(20, true);
     },
-    get footnoteTableBlockLength() {
+    get footnoteTableLength() {
       return PAGE_BYTES * (this.dv.getUint16(22, true) - this.dv.getUint16(20, true));
     },
-    get sectionPropertyOffset() {
+    get sectionTableOffset() {
       return PAGE_BYTES * this.dv.getUint16(22, true);
     },
-    get sectionTableBlockLength() {
+    get sectionTableLength() {
       return PAGE_BYTES * (this.dv.getUint16(24, true) - this.dv.getUint16(22, true));
     },
     get pageTableOffset() {
       return PAGE_BYTES * this.dv.getUint16(26, true);
     },
-    get pageTableBlockLength() {
+    get pageTableLength() {
       return PAGE_BYTES * (this.dv.getUint16(28, true) - this.dv.getUint16(26, true));
     },
     get fontNameTableOffset() {
       return PAGE_BYTES * this.dv.getInt16(28, true);
     },
-    get fontNameTableBlockLength() {
+    get fontNameTableLength() {
       return PAGE_BYTES * (this.dv.getInt16(96, true) - this.dv.getInt16(28, true));
     },
     // 66 bytes reserved (Word compatibility)
-    get totalBlockLength() {
+    get totalLength() {
       return PAGE_BYTES * this.dv.getInt16(96, true);
     },
     // reserved uint16 x15
   };
-  HeaderView.byteLength = PAGE_BYTES;
+  FileHeaderView.byteLength = PAGE_BYTES;
   
   return {
-    FileHeaderView: WriteFileHeaderView,
+    split: split,
   };
 
 });
