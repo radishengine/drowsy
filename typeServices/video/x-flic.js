adc2021a56447f8648973b53b6f72b4b0240bf1e
radishengine <radishengine@gmail.com>
2016-09-28 18:41:18 +0100

    Begin FLIC splitter (only header so far)

---
+++ typeServices/video/x-flic.js
@@ -0,0 +1,106 @@
+define(['../../msdos/util'], function(dosUtil) {
+
+  'use strict';
+  
+  function split(entries) {
+    var rootSegment = this;
+    var headerSegment = rootSegment.getSegment('video/x-flic-header', 0, HeaderView.byteLength);
+    return headerSegment.getBytes().then(function(rawHeader) {
+      var header = new HeaderView(rawHeader.buffer, rawHeader.byteOffset, rawHeader.byteLength);
+      if (entries.accepted('video/x-flic-header')) {
+        entries.add(headerSegment);
+      }
+      var pos = HeaderView.byteLength;
+    });
+  }
+  
+  function HeaderView(buffer, byteOffset, byteLength) {
+    this.dv = new DataView(buffer, byteOffset, byteLength);
+  }
+  HeaderView.prototype = {
+    get totalByteLength() {
+      return this.dv.getUint32(0, true);
+    },
+    get fileType() {
+      var value = this.dv.getUint16(4, true);
+      switch (value) {
+        case 0xAF11: return 'fli';
+        case 0xAF12: return 'flic8BitDepth';
+        case 0xAF44: return 'flicNon8BitDepth';
+        case 0xAF30: return 'flicHuffmanOrBWTCompression';
+        case 0xAF31: return 'flicFrameShiftCompression';
+        default: return value;
+      }
+    },
+    get frameCountInFirstSegment() {
+      return this.dv.getUint16(6, true);
+    },
+    get width() {
+      return this.dv.getUint16(8, true);
+    },
+    get height() {
+      return this.dv.getUint16(10, true);
+    },
+    get bitsPerPixel() {
+      return this.dv.getUint16(12, true);
+    },
+    get flags() {
+      return this.dv.getUint16(14, true);
+    },
+    get frameDuration() {
+      var value = this.dv.getUint32(16, true);
+      return this.fileType === 'fli' ? (value * 1000)/70 : value; // millisecond conversion
+    },
+    // 2 reserved bytes
+    get createdAt() {
+      return dosUtil.getTimeAndDate(this.dv, 22); // FLC only
+    },
+    get creatorID() {
+      return this.dv.getUint32(26, true); // FLC only
+    },
+    get updatedAt() {
+      return dosUtil.getTimeAndDate(this.dv, 30); // FLC only
+    },
+    get updaterID() {
+      return this.dv.getUint32(34, true); // FLC only
+    },
+    get aspectWidth() {
+      return this.dv.getUint16(38, true); // FLC only
+    },
+    get aspectHeight() {
+      return this.dv.getUint16(40, true); // FLC only
+    },
+    get egiExtensionFlags() {
+      return this.dv.getUint16(42, true); // EGI only
+    },
+    get keyFrameFrequency() {
+      return this.dv.getUint16(44, true); // EGI only
+    },
+    get frameCount() {
+      return this.dv.getUint16(46, true); // EGI only
+    },
+    get maxUncompressedChunkSize() {
+      return this.dv.getUint32(48, true); // EGI only
+    },
+    get maxRegionsInCHK_REGION() {
+      return this.dv.getUint16(52, true); // EGI only
+    },
+    get transparentLevelCount() {
+      return this.dv.getUint16(54, true);
+    },
+    get frameOffset1() {
+      return this.dv.getUint32(80, true); // FLC only
+    },
+    get frameOffset2() {
+      return this.dv.getUint32(84, true); // FLC only
+    },
+    // reserved: 40 bytes
+  };
+  HeaderView.byteLength = 128;
+  
+  return {
+    split: split,
+    bytePattern: /^.{4}[\x11\x12\x44\x30\x31]\xAF/,
+  };
+
+});
