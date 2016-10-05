24e1800d6d055888795fcd00750af1be208f97e6
radishengine <radishengine@gmail.com>
2016-10-05 12:22:47 +0100

    Add mount function

--- typeServices/application/zip.js
+++ typeServices/application/zip.js
@@ -2,6 +2,52 @@ define(['DataSegment'], function(DataSegment) {
 
   'use strict';
   
+  function mount(zip, volume) {
+    var promises = [];
+    return zip.split(
+      function(entry) {
+        if (entry.getTypeParameter('type') !== 'local') return;
+        var offset = +entry.getTypeParameter('offset');
+        var headerSegment = entry.getSegment(entry.type, 0, offset);
+        promises.push(headerSegment.getStruct().then(function(record) {
+          var ext = record.path.match(/[^\.]*$/)[0].toLowerCase();
+          var type = dispatch.byExtension[ext] || 'application/octet-stream';
+          if (record.compressionMethod !== 'none') {
+            var innerType = type;
+            switch (record.compressionMethod) {
+              case 'shrunk': type = 'application/x-lzw; variant=shrink'; break;
+              case 'factor1': type = 'application/x-reduced; factor=1'; break;
+              case 'factor2': type = 'application/x-reduced; factor=2'; break;
+              case 'factor3': type = 'application/x-reduced; factor=3'; break;
+              case 'factor4': type = 'application/x-reduced; factor=4'; break;
+              case 'imploded': type = 'application/x-ibm-terse; variant=old'; break;
+              case 'deflated': type = 'application/x-deflated'; break;
+              case 'enhancedDeflated': type = 'application/x-deflated; variant=deflate64'; break;
+              case 'dclImploded': type = 'application/x-imploded; variant=dcl'; break;
+              case 'bzip2': type = 'application/x-bzip2'; break;
+              case 'lzma': type = 'application/x-lzma'; break;
+              case 'terse': type = 'application/x-ibm-terse; variant=new'; break;
+              case 'lz77': type = 'application/x-lz77'; break;
+              case 'jpeg': type = 'image/jpeg'; break;
+              case 'wavpack': type = 'audio/x-wavpack'; break;
+              case 'ppmd': type = 'application/x-ppmd; version=i; rev=1'; break;
+              case 'aes': type = 'application/x-aes'; break;
+              default: return Promise.reject('unknown compression: ' + record.compressionMethod);
+            }
+            if (innerType !== 'application/octet-stream') {
+              type += '; type='+innerType;
+            }
+            var full = record.uncompressedByteLength32; // TODO: zip64
+            type += '; full='+full;
+          }
+          volume.addFile(record.path, entry.getSegment(type, offset));
+        }));
+      },
+      function() {
+        return Promise.all(promises);
+      });
+  }
+  
   var TRAILER_TYPE = 'chunk/zip; type=trailer';
   var CENTRAL_RECORD_TYPE = 'chunk/zip; type=central';
   var LOCAL_RECORD_TYPE = 'chunk/zip; type=local';
@@ -443,6 +489,7 @@ define(['DataSegment'], function(DataSegment) {
   return {
     split: split,
     join: join,
+    mount: mount,
     bytePattern: /PK\x05\x06.{18}.{0,65535}$/,
   };
 
