462376d399479aca93a83f7e7c7e857157664bb1
radishengine <radishengine@gmail.com>
2016-10-05 19:34:06 +0100

    Don't recursively split

--- typeServices/volume/optical-media.js
+++ typeServices/volume/optical-media.js
@@ -10,14 +10,14 @@ define(function() {
       if (String.fromCharCode(sig[0], sig[1], sig[2], sig[3]) !== 'PM\0\0') {
         return;
       }
-      return segment.getSegment('volume/mac-partitioned').split();
+      entries.add(segment.getSegment('volume/mac-partitioned'));
     }, NO_OP);
     
     var iso9660 = segment.getBytes(2048 * 16 + 1, 5).then(function(sig) {
       if (String.fromCharCode(sig[0], sig[1], sig[2], sig[3], sig[4]) !== 'CD001') {
         return;
       }
-      return segment.getSegment('volume/iso-9660').split();
+      entries.add(segment.getSegment('volume/iso-9660'));
     }, NO_OP);
     
     return Promise.all([
