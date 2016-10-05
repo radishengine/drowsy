96e4ddec575546bc2d62c0eabb9494fb7f507fa3
radishengine <radishengine@gmail.com>
2016-10-05 23:44:38 +0100

    Recursively split only on ambiguous volumes

--- typeServices/volume/mac-partitioned.js
+++ typeServices/volume/mac-partitioned.js
@@ -5,18 +5,30 @@ define(function() {
   function split(segment, entries) {
     function doPartition(n) {
       var partitionRecord = byteSource.getSegment('chunk/mac-partition-map', 512 * n, 512);
+      var promises = [];
       return partitionRecord.getStruct().then(function(partition) {
         if (!partition.hasValidTag) {
           return Promise.reject('invalid partition map signature');
         }
         entries.add(partitionRecord);
-        entries.add(segment.getSegment(
+        var partitionSegment = segment.getSegment(
           partitionRecord.partitionSegmentType,
           512 * partitionRecord.blockOffset,
-          512 * partitionRecord.blockCount));
+          512 * partitionRecord.blockCount);
+        if (partitionSegment.typeName === 'volume/ambiguous') {
+          promises.push(partitionSegment.split(function(entry) {
+            entries.add(entry);
+          }));
+        }
+        else {
+          entries.add(partitionSegment);
+        }
         if (n < partition.totalPartitionCount) {
           return doPartition(n + 1);
         }
+      })
+      .then(function() {
+        return Promise.all(promises);
       });
     }
     return doPartition(1);
