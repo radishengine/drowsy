3e6989d0c58d913dafec8ab9a66b2fcd93687a60
radishengine <radishengine@gmail.com>
2016-10-06 12:03:29 +0100

    Change the way parts are extracted

--- typeServices/volume/mac-hfs.js
+++ typeServices/volume/mac-hfs.js
@@ -1,6 +1,20 @@
-define(function() {
+define(['DataSegment'], function(DataSegment) {
   
   'use strict';
+  
+  function getSegmentFromExtents(allocationSegment, chunkSize, type, byteLength, extents) {
+    if (byteLength === 0) return new DataSegment.Empty(type);
+    var list = [], i = 0;
+    do {
+      if (i > extents.length) throw new Error('insufficient space in extents');
+      var offset = chunkSize * extents[i].offset, len = Math.min(byteLength, chunkSize * extents[i].length);
+      list.push(allocationSegment.getSegment(allocationSegment.type, offset, len));
+      byteLength -= len;
+      i++;
+    } while (byteLength > 0);
+    if (list.length === 1) return list[0];
+    return DataSegment.from(list, type);
+  }
   
   function split(segment, entries) {
     var mdbSegment = segment.getSegment('chunk/mac-hfs; which=master-directory-block', 512 * 2, 512);
@@ -10,44 +24,30 @@ define(function() {
         return Promise.reject('not an HFS volume');
       }
       entries.add(mdbSegment);
-      var allocationType = 'chunk/mac-hfs; part=allocation';
+      var allocationType = 'chunk/mac-hfs; which=allocation';
       var allocChunkSize = masterDirectoryBlock.allocationChunkByteLength;
       allocationType += '; chunk=' + allocChunkSize;
-      function extentString(byteLength, extents) {
-        var list = [];
-        while (byteLength > 0) {
-          var extent = extents.shift();
-          if (!extent) {
-            throw new Error('insufficient space given by extents');
-          }
-          var length = Math.min(byteLength, extent.length * allocChunkSize);
-          list.push('(' + (extent.offset * allocChunkSize) + ',' + length + ')');
-          byteLength -= length;
-        }
-        return list.join('');
-      }
-      if (masterDirectoryBlock.catalogFileByteLength > 0) {
-        allocationType += '; catalog=' + extentString(
-          masterDirectoryBlock.catalogFileByteLength,
-          masterDirectoryBlock.catalogFileExtentRecord);
-      }
-      if (masterDirectoryBlock.extentsOverflowFileByteLength > 0) {
-        allocationType += '; overflow=' + extentString(
-          masterDirectoryBlock.extentsOverflowFileByteLength,
-          masterDirectoryBlock.extentsOverflowFileExtentRecord);
-      }
-      var allocationSegment = segment.getSegment(allocationType,
+      var allocSegment = segment.getSegment(allocationType,
         masterDirectoryBlock.firstAllocationBlock,
         masterDirectoryBlock.allocationChunkCount * masterDirectoryBlock.allocationChunkByteLength);
-      entries.add(allocationSegment);
+      entries.add(getSegmentFromExtents(
+        allocSegment,
+        allocChunkSize,
+        'volume/mac-hfs; part=catalog',
+        masterDirectoryBlock.catalogFileByteLength,
+        masterDirectoryBlock.catalogFileExtentRecord));
+      entries.add(getSegmentFromExtents(
+        allocSegment,
+        allocChunkSize,
+        'volume/mac-hfs; part=overflow',
+        masterDirectoryBlock.extentsOverflowFileByteLength,
+        masterDirectoryBlock.extentsOverflowFileExtentRecord));
     });
   }
   
   function mount(segment, volume) {
-    return segment.split(function(entry) {
-      if (entry.typeName === 'volume/hfs' && entry.getTypeParameter('part') === 'allocation') {
-        console.log(entry.type);
-      }
+    return segment.split().then(function(parts) {
+      console.log(parts);
     });
   }
   
