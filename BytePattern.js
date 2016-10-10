8060091250e78cc9bcf7e26e51d24f869a458d24
radishengine <radishengine@gmail.com>
2016-10-10 20:13:53 +0100

    Create BytePattern.js

---
+++ BytePattern.js
@@ -0,0 +1,40 @@
+define(function() {
+
+  'use strict';
+  
+  var rx_escape = '\\\\(?:u\\{[^\\}]+\\}?|.)';
+  var rx_set = '\\[(?:[^\\\\\\]]+|' + rx_escape + ')+\\]';
+  var rx_singleton = rx_escape + '|' + rx_set + '|[^\\\\\\[\\]\\(\\)\\{\\}\\*\\?\\+\\$\\^\\|]+';
+  var rx_special = '\\((?:\?[=!:])?|\\)|\\*|\\+|\\{[^\\}]*\\}|\\$|\\^';
+  
+  var rx = new RegExp('(' + rx_singleton + ')|(' + rx_hot + ')', 'g');
+  
+  function BytePattern(source) {
+    if (regexp instanceof RegExp) source = source.source;
+    else if (typeof source !== 'string') {
+      throw new TypeError('source must be string or RegExp');
+    }
+    rx.lastIndex = 0;
+    var nextIndex = 0;
+    for (var match = rx.exec(source); match; match = rx.exec(source)) {
+      if (match.index > nextIndex) {
+        throw new Error('invalid pattern: ' + source);
+      }
+      if (typeof match[1] === 'string') {
+        // character set or block of literal characters
+        var ch = match[1];
+      }
+      else {
+        // special character (grouping, repeat, etc.)
+        var special = match[2];
+        console.log('special', special);
+      }
+    }
+    if (nextIndex < source.length) {
+      throw new Error('invalid pattern: ' + source);
+    }
+  }
+  
+  return BytePattern;
+
+});
