e67a0c4df5c415cc4291ffa0046447c39c36196b
radishengine <radishengine@gmail.com>
2016-10-13 15:16:25 +0100

    Update TypeDescriptor.js

--- TypeDescriptor.js
+++ TypeDescriptor.js
@@ -43,9 +43,143 @@ define(function() {
     return obj === null ? EMPTY : Object.freeze(obj);
   }
   
+  var matchAny, matchNone;
+  
+  var descriptorFilterCache = new WeakMap();
+  
+  function TypeFilter() {
+    if (this) return; // do nothing if used as a constructor
+    if (arguments.length === 0) return matchAny;
+    if (arguments[0] instanceof TypeFilter) {
+      if (arguments.length !== 1) {
+        if (arguments[0] instanceof TypeDescriptor) {
+          for (var i = 1; i < arguments.length; i++) {
+            if (!(arguments[i] instanceof TypeDescriptor)) {
+              throw new TypeError('filter(descriptor [, descriptor...]): every argument must be a descriptor object');
+            }
+          }
+          return new OrList(Object.freeze(Array.prototype.slice.apply(arguments)));
+        }
+        throw new Error(
+          'Only one filter object is allowed here.'
+          + ' Use .filter(), .or(), .except() etc.'
+          + ' to combine several filters into one object.');
+      }
+      return arguments[0];
+    }
+    var namePattern, parameterPatterns;
+    if (typeof arguments[0] === 'string' || arguments[0] instanceof RegExp) {
+      namePattern = arguments[0];
+      if (namePattern === '*/*') namePattern = ANYSTRING;
+      parameterPatterns = arguments[1] || EMPTY;
+    }
+    else {
+      if (typeof arguments[0] !== 'object') {
+        throw new TypeError('Argument 1: expecting string, regular expression or object');
+      }
+      namePattern = ANYSTRING;
+      parameterPatterns = arguments[0] || EMPTY;
+    }
+    var andList = [];
+    if (typeof namePattern === 'string') {
+      var nameParts = arguments[0].match(/^\s*([^\/]+\/[^\/;]+)(?:\s*;\s*(\S.*)?)?$/);
+      if (!nameParts) {
+        throw new Error('type name must take the form: category/subtype');
+      }
+      namePattern = nameParts[1];
+      var stringParameters = nameParts[2] ? decodeTypeParameters(nameParts[2]) : EMPTY;
+      if (/\*/.test(namePattern)) {
+        namePattern = new RegExp('^' + namePattern.split(/\*/g).map(regexEscape).join('.*') + '$');
+        andList.push(new NameMatch(namePattern));
+        parameterPatterns = Object.assign(stringParameters, parameterPatterns);
+      }
+      else {
+        if (parameterPatterns !== EMPTY) {
+          var keys = Object.keys(parameterPatterns);
+          parameterPatterns = Object.assign({}, parameterPatterns);
+          var count = 0;
+          for (var i = 0; i < keys.length; i++) {
+            var name = keys[i];
+            var value = parameterPatterns[name];
+            if (typeof value === 'string') {
+              stringParameters[name] = value;
+              delete newParameterPatterns[name];
+              count++;
+            }
+          }
+          if (count === keys.length) {
+            parameterPatterns = EMPTY;
+          }
+        }
+        andList.push(new TypeDescriptor(namePattern, stringParameters));
+      }
+    }
+    else {
+      andList.push(new NameMatch(namePattern));
+    }
+    if (parameterPatterns !== EMPTY) {
+      var keys = Object.keys(parameterPatterns);
+      for (var i = 0; i < keys.length; i++) {
+        var parameterName = keys[i];
+        var parameterPattern = parameterPatterns[parameterName];
+        if (parameterPattern === true) {
+          andList.push(new ParameterMatch(parameterName, ANYSTRING));
+        }
+        else if (parameterPattern === false) {
+          andList.push(new ParameterMatch(parameterName, ANYSTRING, true));
+        }
+        else if (parameterPattern instanceof RegExp) {
+          andList.push(new ParameterMatch(parameterName, parameterPattern));
+        }
+        else {
+          parameterPattern = new RegExp('^' + regexEscape('' + parameterPattern) + '$');
+          andList.push(new ParameterMatch(parameterName, parameterPattern));
+        }
+      }
+    }
+    if (andList.length === 0) return matchAny;
+    if (andList.length === 1) return andList[0];
+    return new AndList(Object.freeze(andList));
+  }
+  TypeFilter.prototype = {
+    filter: function() {
+      var filter = TypeFilter.apply(null, arguments);
+      if (filter === matchAny) return this;
+      if (filter === matchNone) return matchNone;
+      if (filter instanceof AndList) {
+        return new AndList(Object.freeze([this].concat(filter.list)));
+      }
+      return new AndList(this, filter);
+    },
+    except: function() {
+      return this.filter(TypeFilter.apply(null, arguments).inverted());
+    },
+    or: function() {
+      var filter = TypeFilter.apply(null, arguments);
+      if (filter === matchAny) return matchAny;
+      if (filter === matchNone) return this;
+      if (filter instanceof OrList) {
+        return new OrList(Object.freeze([this].concat(filter.list)));
+      }
+      return new OrList(this, filter);
+    },
+    reset: function() {
+      return this;
+    },
+    willNeverMatch: false,
+    inverted: function() {
+      return new Not(this);
+    },
+    toString: function() {
+      return JSON.stringify(this);
+    },
+  };
+  
   function TypeDescriptor(typeName, typeParameters) {
     if (!this) {
-      if (arguments[0] instanceof TypeDescriptor) return arguments[0];
+      if (typeName instanceof TypeDescriptor) {
+        return typeName;
+      }
       return new TypeDescriptor(typeName, typeParameters);
     }
     var nameParts = typeName.match(/^\s*([a-z0-9_\-\.]+)\/([a-z0-9_\-\.]+)\s*(?:;(.*))?$/);
@@ -57,7 +191,7 @@ define(function() {
     if (!typeParameters) {
       typeParameters = EMPTY;
     }
-    else {
+    else if (typeParameters !== EMPTY) {
       var keys = Object.keys(typeParameters);
       if (keys.length === 0) {
         typeParameters = EMPTY;
@@ -93,23 +227,13 @@ define(function() {
     this.parameters = typeParameters;
     Object.freeze(this);
   }
-  Object.assign(TypeDescriptor, {
-    encodeString: encodeTypeString,
-    decodeString: decodeTypeString,
-    encodeParameters: encodeTypeParameters,
-    decodeParameters: decodeTypeParameters,
-  });
-  TypeDescriptor.prototype = {
-    get name() {
-      return this.category + '/' + this.subtype;
-    },
+  TypeDescriptor.prototype = Object.assign(new TypeFilter, {
     toString: function() {
       var name = this.name, parameters = encodeTypeParameters(this.parameters);
       return parameters ? (name + '; ' + parameters) : name;
     },
     toJSON: function() {
-      if (this.parameters === EMPTY) return this.name;
-      return [this.name, this.parameters];
+      return this.toString();
     },
     test: function() {
       var other = TypeDescriptor.apply(null, arguments);
@@ -128,78 +252,27 @@ define(function() {
       }
       return true;
     },
-    filter: function() {
-      if (arguments.length === 0) return this;
-      var filter = filter.apply(null, arguments);
-      if (filter instanceof AndList) {
-        return new AndList(Object.freeze([this].concat(filter.list)));
-      }
-      return new AndList(Object.freeze([this, filter]));
+  });
+  Object.defineProperty(TypeDescriptor.prototype, 'name', {
+    get: function() {
+      return this.category + '/' + this.subtype;
     },
-    except: function() {
-      return this.filter(filter.apply(null, arguments).inverted());
-    },
-    or: function() {
-      var filter = filter.apply(null, arguments);
-      if (filter instanceof OrList) {
-        return new OrList(Object.freeze([this].concat(filter.list)));
-      }
-      return new OrList(Object.freeze([this, filter]));
-    },
-    reset: function() {
-      return this;
-    },
-    inverted: function() {
-      return new Not(this);
-    },
-    willNeverMatch: false,
-  };
-  
-  var matchAny, matchNone;
-  
-  function TypeFilter() {
-  }
-  TypeFilter.prototype = {
-    filter: function() {
-      var filter = filter.apply(null, parameters);
-      if (filter === matchAny) return this;
-      if (filter === matchNone) return matchNone;
-      return new AndList(this, filter);
-    },
-    except: function() {
-      return this.filter(filter.apply(null, parameters).inverted());
-    },
-    or: function() {
-      var filter = filter.apply(null, parameters);
-      if (filter === matchAny) return matchAny;
-      if (filter === matchNone) return this;
-      return new OrList(this, filter);
-    },
-    reset: function() {
-      return this;
-    },
-    willNeverMatch: false,
-    inverted: function() {
-      return new Not(this);
-    },
-  };
+  });
   
   matchAny = Object.freeze(Object.assign(new TypeFilter, {
-    filter: function() { return filter.apply(null, arguments); },
-    except: function() { return filter.apply(null, arguments).inverted(); },
+    filter: function() { return TypeFilter.apply(null, arguments); },
+    except: function() { return TypeFilter.apply(null, arguments).inverted(); },
     inverted: function() { return matchNone; },
     or: function() { return this; },
     toJSON: function() { return true; },
-    test: function(descriptor) {
-      return true;
-    },
+    test: function() { return true; },
   }));
   
   matchNone = Object.freeze(Object.assign(new TypeFilter, {
     filter: function() { return matchNone; },
     except: function() { return matchNone; },
     inverted: function() { return matchAny; },
-    or: function() { return filter.apply(null, arguments); },
+    or: function() { return TypeFilter.apply(null, arguments); },
     toJSON: function() { return false; },
     test: function() { return false; },
     count: function() { return matchNone; },
@@ -209,38 +282,29 @@ define(function() {
   function AndList(list) {
     if (Array.isArray(list)) {
       if (!Object.isFrozen(list)) {
-        list = Object.freeze(list.map(filter);
-      }
-      else {
-        for (var i = 0; i < list.length; i++) {
-          if (list[i] instanceof TypeFilter || list[i] instanceof TypeDescriptor) {
-            continue;
-          }
-          list = Object.freeze(list.map(filter));
-          break;
-        }
+        list = Object.freeze(list.slice());
       }
     }
     else {
-      list = Object.freeze(Array.prototype.map.apply(arguments, filter));
+      list = Object.freeze(Array.slice.apply(arguments));
     }
     this.list = list;
     Object.freeze(this);
   }
   AndList.prototype = Object.assign(new TypeFilter, {
     filter: function() {
-      var filter = filter.apply(null, arguments);
-      if (filter === none) return none;
-      if (filter === all) return this;
+      var filter = TypeFilter.apply(null, arguments);
+      if (filter === matchNone) return matchNone;
+      if (filter === matchAny) return this;
       if (filter instanceof AndList) {
         return new AndList(this.list.concat(filter.list));
       }
       return new AndList(this.list.concat(filter));
     },
     inverted: function() {
-      return new OrList(this.list.map(function(filter) {
+      return new OrList(Object.freeze(this.list.map(function(filter) {
         return filter.inverted();
-      }));
+      })));
     },
     reset: function() {
       var newList, resetElement;
@@ -250,9 +314,10 @@ define(function() {
           newList[i] = resetElement;
         }
       }
-      return newList ? new AndList(newList) : this;
+      return newList ? new AndList(Object.freeze(newList)) : this;
     },
-    test: function(descriptor) {
+    test: function() {
+      var descriptor = TypeDescriptor.apply(null, arguments);
       for (var i = 0; i < this.list.length; i++) {
         if (!this.list[i].test(descriptor)) return false;
       }
@@ -271,38 +336,29 @@ define(function() {
   function OrList(list) {
     if (Array.isArray(list)) {
       if (!Object.isFrozen(list)) {
-        list = Object.freeze(list.map(filter);
-      }
-      else {
-        for (var i = 0; i < list.length; i++) {
-          if (list[i] instanceof TypeFilter || list[i] instanceof TypeDescriptor) {
-            continue;
-          }
-          list = Object.freeze(list.map(filter));
-          break;
-        }
+        list = Object.freeze(list.slice());
       }
     }
     else {
-      list = Object.freeze(Array.prototype.map.apply(arguments, filter));
+      list = Object.freeze(Array.prototype.slice.apply(arguments));
     }
     this.list = list;
     Object.freeze(this);
   }
-  OrList.prototype = Object.assign(new TypeFilter(EMPTY), {
+  OrList.prototype = Object.assign(new TypeFilter, {
     or: function() {
-      var filter = filter.apply(null, arguments);
-      if (filter === all) return all;
-      if (filter === none) return this;
+      var filter = TypeFilter.apply(null, arguments);
+      if (filter === matchAny) return matchAny;
+      if (filter === matchNone) return this;
       if (filter instanceof OrList) {
         return new OrList(this.list.concat(filter.list));
       }
       return new OrList(this.list.concat(filter));
     },
     inverted: function() {
-      return new AndList(this.list.map(function(filter) {
+      return new AndList(Object.freeze(this.list.map(function(filter) {
         return filter.inverted();
-      }));
+      })));
     },
     reset: function() {
       var newList, resetElement;
@@ -314,7 +370,8 @@ define(function() {
       }
       return newList ? new OrList(newList) : this;
     },
-    test: function(descriptor) {
+    test: function() {
+      var descriptor = TypeDescriptor.apply(null, arguments);
       for (var i = 0; i < this.list.length; i++) {
         if (this.list[i].test(descriptor)) return true;
       }
@@ -337,7 +394,7 @@ define(function() {
   }
   Not.prototype = Object.assign(new TypeFilter, {
     test: function() {
-      return !this.filter.test.apply(this.test, arguments);
+      return !this.filter.test.apply(this.filter, arguments);
     },
     inverted: function() {
       return this.filter;
@@ -361,7 +418,8 @@ define(function() {
   }
   NameMatch.prototype = Object.assign(new TypeFilter, {
     isInverted: false,
-    test: function(descriptor) {
+    test: function() {
+      var descriptor = TypeDescriptor.apply(null, arguments);
       if (this.isInverted) {
         return !this.pattern.test(descriptor.name);
       }
@@ -382,7 +440,8 @@ define(function() {
   }
   ParameterMatch.prototype = Object.assign(new TypeFilter, {
     isInverted: false,
-    test: function(descriptor) {
+    test: function() {
+      var descriptor = TypeDescriptor.apply(null, arguments);
       if (this.isInverted) {
         if (this.name in descriptor.properties) {
           return true;
@@ -412,10 +471,11 @@ define(function() {
   }
   CountedMatch.prototype = Object.assign(new TypeFilter, {
     test: function() {
-      if (this.count < 1) return false;
+      if (this.count < 1) return this.isInverted;
       var result = this.filter.test.apply(this.filter, arguments);
+      if (this.isInverted) result = !result;
       if (result) this.count--;
-      return this.isInverted ? result : !result;
+      return result;
     },
     reset: function() {
       return new CountedMatch(this.filter, this.startCount, this.isInverted);
@@ -430,106 +490,26 @@ define(function() {
     },
   });
   
-  var descriptorFilterCache = new WeakMap();
-  
-  function filter(namePattern, parameterPatterns) {
-    if (arguments.length === 0) return matchAny;
-    if (arguments[0] instanceof TypeFilter) {
-      if (arguments.length !== 1) {
-        throw new Error('Only one filter object is allowed. Use .filter(), .or(), .except() etc. to combine several into one.');
-      }
-      return arguments[0];
-    }
-    if (namePattern instanceof TypeDescriptor) {
-      for (var i = 1; i < arguments.length; i++) {
-        if (!(arguments[i] instanceof TypeDescriptor)) {
-          throw new TypeError('filter(descriptor [, descriptor...]): every argument must be a descriptor object');
-        }
-      }
-      if (arguments.length === 1) return arguments[0];
-      return OrList(Object.freeze(Array.prototype.slice.apply(arguments)));
-    }
-    if (typeof namePattern === 'string' && !/\*/.test(namePattern)) {
-      tryDescriptor: do {
-        if (parameterPatterns) {
-          var keys = Object.keys(parameterPatterns);
-          for (var i = 0; i < keys.length; i++) {
-            if (typeof parameterPatterns[keys[i]] !== 'string') {
-              break tryDescriptor;
-            }
-          }
-        }
-        return new TypeDescriptor(namePattern, parameterPatterns);
-      } while (false); // tryDescriptor
-    }
-    if (typeof namePattern === 'object') {
-      parameterPatterns = namePattern;
-      namePattern = ANYSTRING;
-    }
-    var andList = [];
-    if (typeof namePattern === 'string') {
-      var nameParts = namePattern.match(/^\s*([^\/]+\/[^\/;]+)(?:\s*;\s*(\S.*)?)?$/);
-      if (!nameParts) {
-        throw new Error('type name must take the form: category/subtype');
-      }
-      if (nameParts[1] !== '*/*') {
-        if (!/\*/.test(nameParts[1])) {
-          andList.push(new TypeDescriptor(nameParts[1]));
-        }
-        else {
-          namePattern = new RegExp('^' + nameParts[1].split(/\*/g).map(regexEscape).join('.*') + '$');
-          andList.push(new NameMatch(namePattern));
-        }
-      }
-      if (typeof nameParts[2] === 'string') {
-        var extraParameterPatterns = decodeTypeParameters(nameParts[2]);
-        var keys = Object.keys(extraParameterPatterns);
-        for (var i = 0; i < keys.length; i++) {
-          var name = keys[i];
-          var pattern = extraParameterPatterns[name];
-          andList.push(new ParameterMatch(name, new RegExp('^' + regexEscape(pattern) + '$')));
-        }
-      }
-    }
-    else if (namePattern !== ANYSTRING) {
-      filter = filter.filter(new NameMatch(namePattern));
-    }
-    if (parameterPatterns) {
-      var keys = Object.keys(parameterPatterns);
-      for (var i = 0; i < keys.length; i++) {
-        var parameterName = keys[i];
-        var parameterPattern = parameterPatterns[parameterName];
-        if (parameterPattern === true) {
-          andList.push(new ParameterMatch(parameterName, ANYSTRING));
-        }
-        else if (parameterPattern === false) {
-          andList.push(new ParameterMatch(parameterName, ANYSTRING, true));
-        }
-        else if (parameterPattern instanceof RegExp) {
-          andList.push(new ParameterMatch(parameterName, parameterPattern));
-        }
-        else {
-          parameterPattern = new RegExp('^' + regexEscape('' + parameterPattern) + '$');
-          andList.push(new ParameterMatch(parameterName, parameterPattern));
-        }
-      }
-    }
-    if (andList.length === 0) return matchAny;
-    if (andList.length === 1) return andList[0];
-    return new AndList(Object.freeze(andList));
-  }
-  
-  TypeDescriptor.Filter = TypeFilter;
-  TypeDescriptor.filter = filter;
-  TypeDescriptor.except = function() {
-    return filter.apply(null, arguments).inverted();
-  };
-  TypeDescriptor.any = matchAny;
-  TypeDescriptor.none = matchNone;
-  TypeDescriptor.count = function(number) {
-    return matchAny.count(number);
-  };
-  
-  return TypeDescriptor;
+  return Object.assign(TypeDescriptor, {
+    encodeString: encodeTypeString,
+    decodeString: decodeTypeString,
+    encodeParameters: encodeTypeParameters,
+    decodeParameters: decodeTypeParameters,
+    filter: TypeFilter,
+    Filter: TypeFilter,
+    except: function() {
+      return TypeFilter.apply(null, arguments).inverted();
+    },
+    all: matchAny,
+    none, matchNone,
+    count: function(number) {
+      return matchAny.count(number);
+    },
+    isDescriptorOrFilter: function(value) {
+      if (value instanceof TypeDescriptor) return 'descriptor';
+      if (value instanceof TypeFilter) return 'filter';
+      return false;
+    },
+  });
  
 });
