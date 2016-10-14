5582ff185f4ce5195a8b9d044736c3f2b32c03a7
radishengine <radishengine@gmail.com>
2016-10-14 19:27:02 +0100

    Fix loop counter

--- mattress.js
+++ mattress.js
@@ -842,8 +842,8 @@ define(function() {
     }
     var hi53 = (hi * 0x100000000 + (lo & 0x7800)).toString(radix).split('');
     var lo11 = (lo & 0x7ff).toString(radix);
-    for (var i = lo11.length - 1, j = hi53.length - 1, carry = 0; i >= 0; ) {
-      var c = parseInt(lo11[i--], radix) + parseInt(hi53[j--], radix) + carry;
+    for (var i = lo11.length - 1, j = hi53.length - 1, carry = 0; i >= 0; i--, j--) {
+      var c = parseInt(lo11[i], radix) + parseInt(hi53[j], radix) + carry;
       hi53[j] = (c % radix).toString(radix);
       carry = (c / radix) | 0;
     }
