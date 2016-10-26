8ae541fd7ac4836b6046d225a601ef4235e686cc
radishengine <radishengine@gmail.com>
2016-10-26 17:32:14 +0100

    Update x-winhelp.js

--- formats/application/x-winhelp.js
+++ formats/application/x-winhelp.js
@@ -3,6 +3,7 @@ define(['Format'], function(Format) {
   'use strict';
   
   var headerFormat = Format('chunk/winhelp', {which:'header'});
+  var directoryHeaderFormat = Format('chunk/winhelp', {which:'directory-header'});
   
   function split(segment, entries) {
     var context = this;
@@ -10,6 +11,10 @@ define(['Format'], function(Format) {
       if (!header.hasValidSignature) {
         return Promise.reject('winhelp file signature not found');
       }
+      return segment.getStruct(directoryHeaderFormat, header.internalDirectoryOffset)
+      .then(function(internalDirectoryHeader) {
+        
+      });
     });
   }
   
