95a8e5602b027b5e83c9afdce47dddc86067ea61
radishengine <radishengine@gmail.com>
2016-10-26 11:24:33 +0100

    Update font-sfnt.js

--- formats/application/font-sfnt.js
+++ formats/application/font-sfnt.js
@@ -1,9 +1,11 @@
-define(function() {
+define(['Format'], function(Format) {
 
   'use strict';
   
+  var offsetTableFormat = Format('chunk/sfnt; which=offset-table');
+  
   function split(segment, entries) {
-    var offsetTableSegment = segment.getSegment('chunk/sfnt; which=offset-table', 0, 12);
+    var offsetTableSegment = segment.getSegment(offsetTableFormat, 0, 12);
     return offsetTableSegment.getStruct().then(function(offsetTable) {
       entries.add(offsetTableSegment);
       return segment.getBytes(12, offsetTable.tableCount * 16)
@@ -14,13 +16,17 @@ define(function() {
           var checksum = dv.getUint32(i*16 + 4, false);
           var byteOffset = dv.getUint32(i*16 + 8, false);
           var byteLength = dv.getUint32(i*16 + 12, false);
-          entries.add(segment.getSegment('chunk/sfnt; which=' + tag + '; checksum=' + checksum.toString(16), byteOffset, byteLength));
+          entries.add(segment.getSegment(
+            Format('chunk/sfnt', {which:tag, checksum:checksum.toString(16)}),
+            byteOffset,
+            byteLength));
         }
       });
     });
   }
   
   return {
+    splitTo: Format.filter('chunk/sfnt'),
     split: split,
   };
 
