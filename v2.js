b9a8ef0434de0952a4a646b59bd85015025faa66
radishengine <radishengine@gmail.com>
2016-10-27 14:14:22 +0100

    Update v2.js

--- v2.js
+++ v2.js
@@ -48,7 +48,7 @@ require(['Volume'], function(Volume) {
     frame.yHandle = frame.appendChild(document.createElement('DIV'));
     frame.yHandle.className = 'y-handle';
     frame.cornerHandle = frame.appendChild(document.createElement('DIV'));
-    frame.cornerHandler.className = 'corner-handle';
+    frame.cornerHandle.className = 'corner-handle';
     frame.titleBar = frame.appendChild(document.createElement('DIV'));
     frame.titleBar.className = 'title-bar';
     frame.closeButton = frame.titleBar.appendChild(document.createElement('BUTTON'));
