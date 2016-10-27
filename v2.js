a78e2a62eb7a087c893fb854cdc832e1605a1e5b
radishengine <radishengine@gmail.com>
2016-10-27 11:54:39 +0100

    Update v2.js

--- v2.js
+++ v2.js
@@ -17,20 +17,17 @@ require(['Volume'], function(Volume) {
     var drop = document.createElement('DIV');
     drop.className = 'drop-outline';
     drop.updatePosition = function(mx, my) {
-      console.log(mx, my, this.offsetWidth, this.offsetHeight);
       this.style.left = Math.max(0, mx - this.offsetWidth / 2) + 'px';
       this.style.top = Math.max(0, my - this.offsetHeight / 2) + 'px';
     };
     
     function onDragOver(e) {
-      console.log(e.type, e.target);
       e.stopPropagation();
       e.preventDefault();
       drop.updatePosition(e.screenX, e.screenY);
     }
     
     function onDragLeave(e) {
-      console.log(e.type, e.target);
       desktop.removeChild(drop);
       desktop.removeEventListener('dragover', onDragOver);
       desktop.removeEventListener('dragleave', onDragLeave);
@@ -39,7 +36,6 @@ require(['Volume'], function(Volume) {
     }
   
     function onDrop(e) {
-      console.log(e.type, e.target);
       desktop.removeChild(drop);
       e.stopPropagation();
       e.preventDefault();
@@ -50,14 +46,13 @@ require(['Volume'], function(Volume) {
     }
     
     function onMouseMove(e) {
-      console.log(e.type, e.target);
       drop.updatePosition(e.screenX, e.screenY);
     }
     
     desktop.addEventListener('dragover', onDragOver);
     desktop.addEventListener('dragleave', onDragLeave);
     desktop.addEventListener('drop', onDrop);
-    //desktop.addEventListener('mousemove', onMouseMove);
+    desktop.addEventListener('mousemove', onMouseMove);
     
     desktop.appendChild(drop);
     drop.updatePosition(e.screenX, e.screenY);
