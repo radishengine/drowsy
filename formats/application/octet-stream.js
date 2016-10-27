6fc726610a3d0b9428583ffb80c2daaef4179e48
radishengine <radishengine@gmail.com>
2016-10-27 16:35:53 +0100

    Create octet-stream.js

---
+++ formats/application/octet-stream.js
@@ -0,0 +1,12 @@
+define(function() {
+
+  'use strict';
+  
+  // very important that this format handler be present & valid
+  // as any file format that doesn't load its own handler will
+  // try to load this instead
+  
+  return {
+  };
+
+});
