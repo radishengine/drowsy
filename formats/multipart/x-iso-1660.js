5b8a932b9589e49570202e4627bc8ed5df81a357
radishengine <radishengine@gmail.com>
2016-10-28 20:20:40 +0100

    Update x-iso-1660.js

--- formats/multipart/x-iso-1660.js
+++ formats/multipart/x-iso-1660.js
@@ -8,15 +8,15 @@ define(['Format'], function(Format) {
   return {
     split: function(segment, entries) {
       function processRecord(blockAddress) {
-        return segment.getSegment(RECORD_FMT, blockAddress * BLOCK_ADDRESS, BLOCK_SIZE)
+        return segment.getSegment(RECORD_FMT, blockAddress * BLOCK_SIZE, BLOCK_SIZE)
         .getStruct().then(function(record) {
           switch (record.descriptorType) {
             case 'volume':
               entries.add(segment.getSegment(['volume/iso-9660', {
-                'struct-segment': (blockAddress * BLOCK_ADDRESS) + ',' + BLOCK_ADDRESS,
+                'root-segment': (blockAddress * BLOCK_SIZE) + ',' + BLOCK_SIZE,
               }]));
               // continue iteration
-              return doVolumeDescriptor(n + 1);
+              return processRecord(blockAddress + 1);
             case 'terminator':
               // prevent further iteration
               return;
