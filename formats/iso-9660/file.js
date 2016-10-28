55bf319871d5938a00dd20f936d17b63d8842460
radishengine <radishengine@gmail.com>
2016-10-28 22:37:18 +0100

    Create file.js

---
+++ formats/iso-9660/file.js
@@ -0,0 +1,45 @@
+define(['Format', 'formats/byExtension'], function(Format, formatsByExtension) {
+
+  'use strict';
+  
+  const RECORD_FMT = Format('iso-9660/chunk', {which:'file'});
+  
+  return {
+    split: function split(segment, entries) {
+      var recordSegment = (segment.format.parameters['record-segment'] || '').match(/^\s*(\d+)\s*,\*s(\d+)\s*$/);
+      if (!recordSegment) {
+        return Promise.reject('missing or invalid record-segment parameter');
+      }
+      recordSegment = segment.getSegment(RECORD_FMT, +recordSegment[1], +recordSegment[2]);
+      entries.add(recordSegment);
+      return recordSegment.getStruct().then(function(record) {
+        var ext = record.name.match(/\.([^\.]+)$/);
+        ext = ext && ext[1];
+        var dataFormat = formatsByExtension[ext || ''] || Format.generic;
+        var dataSegment = (segment.format.parameters['data-segment'] || '').match(/^\s*(\d+)\s*,\*s(\d+)\s*$/);
+        if (!dataSegment) {
+          return Promise.reject('missing or invalid data-segment parameter');
+        }
+        dataSegment = segment.getSegment(dataFormat, +dataSegment[1], +dataSegment[2]);
+        entries.add(dataSegment);
+      });
+    },
+    getDisplayName: function(segment) {
+      return segment.split(RECORD_FMT).then(function(records) {
+        if (records.length === 0) {
+          return Promise.reject('split failed to find info record');
+        }
+        return records[0].name;
+      });
+    },
+    getTimestamp: function(segment) {
+      return segment.split(RECORD_FMT).then(function(records) {
+        if (records.length === 0) {
+          return Promise.reject('split failed to find info record');
+        }
+        return records[0].recordedAt;
+      });
+    },
+  };
+
+});
