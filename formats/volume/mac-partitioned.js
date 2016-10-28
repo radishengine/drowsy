5f611ad687655d659616dab9bff8acc8c076b392
radishengine <radishengine@gmail.com>
2016-10-29 00:17:05 +0100

    Update mac-partitioned.js

--- formats/volume/mac-partitioned.js
+++ formats/volume/mac-partitioned.js
@@ -1,16 +1,20 @@
-define(function() {
+define(['Format'], function(Format) {
 
   'use strict';
   
+  const BLOCK_SIZE = 512;
+  const PARTITIONS_FMT = Format('chunk/mac-partition-map');
+  
   function split(segment, entries) {
     function doPartition(n) {
-      var partitionMapSegment = segment.getSegment('chunk/mac-partition-map', 512 * n, 512);
+      var partitionMapSegment = segment.getSegment(PARTITIONS_FMT, BLOCK_SIZE * n, BLOCK_SIZE);
       var promises = [];
       return partitionMapSegment.getStruct().then(function(partitionInfo) {
         if (!partitionInfo.hasValidTag) {
           return Promise.reject('invalid partition map signature');
         }
         entries.add(partitionMapSegment);
+        /*
         var partitionSegment = segment.getSegment(
           partitionInfo.partitionSegmentType,
           512 * partitionInfo.blockOffset,
@@ -23,6 +27,7 @@ define(function() {
         else {
           entries.add(partitionSegment);
         }
+        */
         if (n < partitionInfo.totalPartitionCount) {
           return doPartition(n + 1);
         }
@@ -34,31 +39,8 @@ define(function() {
     return doPartition(1);
   }
   
-  function mount(segment, volume) {
-    return segment.split().then(function(partitions) {
-      return Promise.all(partitions.map(function(partition) {
-        return partition.getCapabilities()
-        .then(function(capabilities) {
-          return (capabilities.mount) ? partition : null;
-        });
-      }));
-    })
-    .then(function(mountables) {
-      mountables = mountables.filter(function(v){ return v !== null; });
-      if (mountables.length === 0) {
-        return Promise.reject('no mountable partition found');
-      }
-      if (mountables.length > 1) {
-        console.log('multiple mountable partitions found:', mountables);
-        console.log('(using the first one)');
-      }
-      return mountables[0].mount(volume);
-    });
-  }
-  
   return {
     split: split,
-    mount: mount,
   };
 
 });
