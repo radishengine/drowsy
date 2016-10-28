776ecad6016b8856b042a998de932979fb55d1fe
radishengine <radishengine@gmail.com>
2016-10-28 22:43:07 +0100

    Update partitioned.js

--- formats/iso-9660/partitioned.js
+++ formats/iso-9660/partitioned.js
@@ -3,18 +3,21 @@ define(['Format'], function(Format) {
   'use strict';
   
   const BLOCK_SIZE = 2048;
-  const RECORD_FMT = Format('chunk/iso-9660', {which:'volume-descriptor'});
+  const RECORD_FMT = Format('iso-9660/chunk', {which:'volume-descriptor'});
   
   return {
     split: function(segment, entries) {
       function processRecord(blockAddress) {
-        return segment.getSegment(RECORD_FMT, blockAddress * BLOCK_SIZE, BLOCK_SIZE)
-        .getStruct().then(function(record) {
+        return segment
+        .getSegment(RECORD_FMT, blockAddress * BLOCK_SIZE, BLOCK_SIZE)
+        .getStruct()
+        .then(function(record) {
           switch (record.descriptorType) {
             case 'volume':
-              entries.add(segment.getSegment(['volume/iso-9660', {
+              var format = Format('iso-9660/volume', {
                 'root-segment': (blockAddress * BLOCK_SIZE) + ',' + BLOCK_SIZE,
-              }]));
+              });
+              entries.add(segment.getSegment(format));
               // continue iteration
               return processRecord(blockAddress + 1);
             case 'terminator':
